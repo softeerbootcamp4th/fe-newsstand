4b71e155398070ae5822ec40f6f087b52ed3f8c4
bluejoyq <bluejoy32@gmail.com>
2024-07-02 16:35:48 +0900

    feat: module.css 타입 적용 및 추가

--- src/components/AppHeader/index.ts
+++ src/components/AppHeader/index.ts
@@ -2,10 +2,10 @@ import { Header, Span } from "../../libs/Elements";
 import { formatDate } from "../../utils/formatDate";
 import { Icon } from "./Icon";
 
-import "./index.css";
+import styles from "./index.module.css";
 export const AppHeader = () => {
   return Header({
-    className: "container",
+    className: styles.container,
     children: [
       Span({
         children: [Icon(), "뉴스스탠드"],
