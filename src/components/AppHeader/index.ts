cb07e7cb3d8b2c80d96318b6efbc1a7a0a17be69
bluejoyq <bluejoy32@gmail.com>
2024-07-02 16:41:30 +0900

    feat: typo 스타일 추가

--- src/components/AppHeader/index.ts
+++ src/components/AppHeader/index.ts
@@ -1,13 +1,14 @@
 import { Header, Span } from "../../libs/Elements";
 import { formatDate } from "../../utils/formatDate";
 import { Icon } from "./Icon";
-
+import typoStyles from "../../styles/typo.module.css";
 import styles from "./index.module.css";
 export const AppHeader = () => {
   return Header({
-    className: styles.container,
+    className: `${styles.container}`,
     children: [
       Span({
+        className: `${typoStyles["display-bold24"]}`,
         children: [Icon(), "뉴스스탠드"],
       }),
       Span({
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { AppHeader } from "./components/AppHeader";
 import { Medias } from "./components/Medias";
 import { TopSection } from "./components/TopSection";
 import { Div } from "./libs/Elements";
-
+import "./index.css";
 export const App = () => {
   return Div({
     children: [AppHeader(), TopSection(), Medias()],
