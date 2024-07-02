40663711cbea7b4585674e8aa806b8182cc3418d
bluejoyq <bluejoy32@gmail.com>
2024-07-02 18:24:35 +0900

    feat: add diffing algorithm to libs

--- src/libs/createApp.ts
+++ src/libs/createApp.ts
@@ -32,11 +32,57 @@ const createApp = () => {
     const doc = parser.parseFromString(rawData, "text/xml");
     return doc.documentElement;
   };
+
+  const _diffingRender = (cur: ChildNode, nxt: ChildNode) => {
+    if (!(cur instanceof HTMLElement && nxt instanceof HTMLElement)) {
+      if (cur.textContent !== nxt.textContent) {
+        cur.replaceWith(nxt);
+      }
+      return;
+    }
+    if (cur.tagName !== nxt.tagName) {
+      cur.replaceWith(nxt);
+      return;
+    }
+
+    const curAttrs = cur.attributes ?? {};
+    const nxtAttrs = nxt.attributes ?? {};
+    for (let i = 0; i < curAttrs.length; i++) {
+      const curAttr = curAttrs[i];
+      const nxtAttr = nxtAttrs.getNamedItem(curAttr.name);
+      if (nxtAttr == null) {
+        cur.removeAttribute(curAttr.name);
+      } else if (curAttr.value !== nxtAttr.value) {
+        curAttr.value = nxtAttr.value;
+      }
+    }
+    for (let i = 0; i < nxtAttrs.length; i++) {
+      const nxtAttr = nxtAttrs[i];
+      if (curAttrs.getNamedItem(nxtAttr.name) == null) {
+        cur.setAttribute(nxtAttr.name, nxtAttr.value);
+      }
+    }
+    const curChildren = cur.childNodes;
+    const nxtChildren = nxt.childNodes;
+    for (let i = 0; i < curChildren.length; i++) {
+      const curChild = curChildren[i];
+      const nxtChild = nxtChildren[i];
+      if (nxtChild == null) {
+        cur.removeChild(curChild);
+      } else {
+        _diffingRender(curChild as HTMLElement, nxtChild as HTMLElement);
+      }
+    }
+    for (let i = curChildren.length; i < nxtChildren.length; i++) {
+      cur.appendChild(nxtChildren[i]);
+    }
+  };
   const render = () => {
     if (root == null || app == null) return;
     statesKey = 0;
     effectsKey = 0;
-    root.innerHTML = "";
+
+    const top = document.createElement("div");
 
     const currentElems: Deque<SimpleElement | RenderedAppElement | RawElement> =
       new Deque();
@@ -44,7 +90,7 @@ const createApp = () => {
 
     while (currentElems.length > 0) {
       const currentElem = currentElems.popFront()!;
-      const parent = currentElem.parent ?? root;
+      const parent = currentElem.parent ?? top;
 
       if (currentElem instanceof SimpleElement) {
         parent.appendChild(document.createTextNode(`${currentElem.value}`));
@@ -59,6 +105,8 @@ const createApp = () => {
       }
       currentElems.pushBack([...currentElem.children]);
     }
+
+    _diffingRender(root, top);
   };
   const useState = <RawT = unknown>(initalState: RawT) => {
     type T = RawT extends unknown ? typeof initalState : RawT;
