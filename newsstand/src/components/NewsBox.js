06ea0265b168afbfdb155071abe81ef3f57fc7fd
admin <admin@adminui-noteubug.local>
2024-07-02 19:39:34 +0900

    fix: id에 props.id가 들어가도록 수정

--- newsstand/src/components/NewsBox.js
+++ newsstand/src/components/NewsBox.js
@@ -12,14 +12,14 @@ const NewsBox = (props) => {
     };
 
     const bindEvents = () => {
-        const newsBox = document.getElementById(`news-box-${props.icon}`); 
+        const newsBox = document.getElementById(`news-box-${props.id}`); 
         newsBox.addEventListener('mouseover', handleMouseOver);
         newsBox.addEventListener('mouseout', handleMouseOut);
     };
 
     return {
         element: `
-        <div class="news-box-container" id="news-box-${props.icon}">
+        <div class="news-box-container" id="news-box-${props.id}">
             ${isHover ? "HOVERED" : props.icon}
         </div>
         `,
