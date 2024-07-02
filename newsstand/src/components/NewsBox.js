6ebc3d963756aa12e6185c12b42e4ad067535d18
admin <admin@adminui-noteubug.local>
2024-07-02 17:50:55 +0900

    feat: NewsBox

--- newsstand/src/components/NewsBox.js
+++ newsstand/src/components/NewsBox.js
@@ -1,10 +1,31 @@
+import useState from '../core/hooks/useState.js';
+
 const NewsBox = (props) => {
-    return{
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseOver = () => {
+        setIsHover(true);
+        console.log(props.icon + " HOVER!");
+    };
+
+    const handleMouseOut = () => {
+        setIsHover(false);
+        console.log(props.icon + " UNHOVER!");
+    };
+
+    const bindEvents = () => {
+        const newsBox = document.getElementById(`news-box-${props.icon}`); 
+        newsBox.addEventListener('mouseover', handleMouseOver);
+        newsBox.addEventListener('mouseout', handleMouseOut);
+    };
+
+    return {
         element: `
-        <div>
-            ${props.title}
+        <div class="news-box-container" id="news-box-${props.icon}">
+            ${isHover ? "HOVERED" : props.icon}
         </div>
         `,
+        bindEvents,
     };
 }
 
