ced73e4f11e56c5771c1f61dc1bd215e76e3a6ce
jhj2713 <juhyojeong58@gmail.com>
2024-07-02 14:37:54 +0900

    feat: 시간 동적으로 적용

---
+++ header/index.js
@@ -0,0 +1,10 @@
+import { getCurrentDate } from "./getCurrentDate.js";
+
+function initHeader() {
+    const currentDate = getCurrentDate();
+
+    const currentDateDOM = document.querySelector('#header__current-date');
+    currentDateDOM.innerText = currentDate;
+}
+
+initHeader();
