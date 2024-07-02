406699b2d12838b63e2221627b2017904f3e2a60
bluejoyq <bluejoy32@gmail.com>
2024-07-02 11:32:46 +0900

    fix: news 모델 수정 및 추가

--- src/models/News.ts
+++ src/models/News.ts
@@ -1,6 +1,16 @@
-import { Media } from "./Media";
-
-export interface RecentNews {
-  media: Media;
+export interface News {
+  href: string;
+  imgSrc: string;
   title: string;
+}
+
+export interface RecentNewsList {
+  data: {
+    mediaId: number;
+    news: News;
+  }[];
+}
+
+export interface MediaRecentNewsList {
+  data: News[];
 }
