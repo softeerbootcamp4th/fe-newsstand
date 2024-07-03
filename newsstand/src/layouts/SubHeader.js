cb83ed5752b583062612a9e43dcf73c95dd74920
admin <admin@adminui-noteubug.local>
2024-07-03 18:08:27 +0900

    SubHeader 레이아웃에 AutoRollingNews 컴포넌트 추가

--- newsstand/src/layouts/SubHeader.js
+++ newsstand/src/layouts/SubHeader.js
@@ -1,9 +1,10 @@
 import createComponent from "../core/component/component.js";
 import AutoRollingNews from "../components/AutoRollingNews.js";
+import { generateRandomId } from "../utils/idGenerator.js";
 
 const SubHeader = () => {
-    const leftAutoRollingNews = createComponent(AutoRollingNews, {id : 1, style : "width: 50%;"});
-    const rightAutoRollingNews = createComponent(AutoRollingNews, {id : 2, style : "width: 50%;"});
+    const leftAutoRollingNews = createComponent(AutoRollingNews, {id : generateRandomId(10), style : "width: 50%;"});
+    const rightAutoRollingNews = createComponent(AutoRollingNews, {id : generateRandomId(10), style : "width: 50%;"});
 
     return {
         element: `
