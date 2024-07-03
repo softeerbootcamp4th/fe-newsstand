952d47e6a60443316ba1c1006f61b0e9c0a2488a
admin <admin@adminui-noteubug.local>
2024-07-03 15:08:48 +0900

    feat: autoRollingNews component

---
+++ newsstand/src/components/AutoRollingNews.js
@@ -0,0 +1,12 @@
+const AutoRollingNews = () => {
+
+    return{
+        element:
+        `<div class="auto-rolling-news-container">
+            <span class="auto-rolling-news-title">연합뉴스</span>
+            <span class="auto-rolling-news-content">[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출</span>
+        </div>`
+    }
+};
+
+export default AutoRollingNews;
