f8198b33e5456d06154a0917d1b90c5eb9ba2f5b
admin <admin@adminui-noteubug.local>
2024-07-03 18:08:04 +0900

    feat: base Button 컴포넌트 구현

--- newsstand/src/components/base/Button.js
+++ newsstand/src/components/base/Button.js
@@ -55,7 +55,7 @@ const Button = (props) => {
     return {
         element: `
         <div class="button" id="button-${props.id}" style="background-color: ${backgroundColor}; color: ${color};">
-            <img class="button-left-icon" src="${props.icon}" alt="icon"/>
+            <img class="button-left-icon" src=${props.icon} alt="icon"/>
             ${props.text}
         </div>
         `,
