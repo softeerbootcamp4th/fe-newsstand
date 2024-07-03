058fa28d7d5be29804c095fbaaa0f653cba3e17a
JSK0406 <12180543@inha.edu>
2024-07-03 16:06:38 +0900

    [feat/4] design: newsBar css 수정

--- components/newsBar/newsBar.js
+++ components/newsBar/newsBar.js
@@ -1,17 +1,17 @@
 const newsBarArea = document.querySelector('#news-bar-area');
+const newsBarWrapper = document.querySelector('#news-bar-wrapper');
+
+const mediaImgPath = '/images/asset 35 1.png'
+const content = '[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출'
 newsBarArea.innerHTML = `
     <div id="news-bar-wrapper" class="flex-row-between">
     </div>
 `
 
-const newsBarWrapper = document.querySelector('#news-bar-wrapper');
-
-const mediaImgPath = '/images/asset 35 1.png'
-const content = 'ABC'
 
 newsBarWrapper.innerHTML = `
     <div id="news-bar" class="flex-row">
-        <img src="${mediaImgPath}" alt="" width="49px" height="17px">
+        <img id="news-bar-media-img" src="${mediaImgPath}" alt="" width="49px" height="17px">
         <p id="news-bar-content">
             <link>
                 ${content}
@@ -19,7 +19,7 @@ newsBarWrapper.innerHTML = `
         </p>
     </div>
     <div id="news-bar" class="flex-row">
-        <img src="${mediaImgPath}" alt="" width="49px" height="17px">
+        <img id="news-bar-media-img" src="${mediaImgPath}" alt="" width="49px" height="17px">
         <p id="news-bar-content">
             <link>
                 ${content}
