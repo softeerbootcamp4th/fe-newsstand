b62cbf58bf25478cb5071220d4a50a1788ecc2c7
g_mi <62610032+akrudal@users.noreply.github.com>
2024-07-03 18:38:34 +0900

    FEATURE - 컴포넌트화 시키기 (#5)

--- newsstand/src/components/alert/alert.js
+++ newsstand/src/components/alert/alert.js
@@ -1,14 +1,21 @@
-export const Alert = (company) => {
-    const pressName = company;
+export const Alert = () => {
 
-    const pressNameElement = document.getElementById('pressName');
-    if (pressNameElement) {
-        pressNameElement.textContent = pressName;
-    } else {
-        console.error('Element with id "pressName" not found.');
-    }
-
-    return document.body.innerHTML;
+    return {
+        element: `
+            <div class ="alert">
+      <div>
+        <div class="alert-text">
+            <span><span class="display-medium16" id ="pressName">언론사명</span> 을(를)</span>
+            <span>구독해지하시겠습니까?</span>
+        </div>
+        <div class="alert-button">
+          <button class="alert-left-button"> 예, 해지합니다 </button>
+          <button class="alert-right-button"> 아니오 </button>
+        </div>
+      </div>
+    </div>
+      `
+      };
 }
 
 export default Alert;
