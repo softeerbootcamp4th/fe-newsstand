65f1f2169795e5c03e48ab0180fa6d5994ae0ad3
jw0097 <jwoo27@hanmail.net>
2024-07-03 15:13:36 +0900

    feat: data attribute를 사용하여 선택된 탭을 구분하는 기능 구현

--- js/category.js
+++ js/category.js
@@ -1,4 +1,30 @@
 import news from "../data/news.js"
 
-console.log(Object.keys(news).length)
+const allMedia = document.querySelector(".news-list__menu__selectors__all");
+const myMedia = document.querySelector(".news-list__menu__selectors__my");
+let selected, unselected;
+
+allMedia.addEventListener("click", onAllClick);
+myMedia.addEventListener("click", onMyclick);
+
+function onAllClick() {
+    allMedia.dataset.selected = "yes";
+    myMedia.dataset.selected = "no";
+    changeSelected();
+}
+
+function onMyclick() {
+    allMedia.dataset.selected = "no";
+    myMedia.dataset.selected = "yes";
+    changeSelected();
+}
+
+function changeSelected() {
+    selected = document.querySelector("span[data-selected='yes']");
+    unselected = document.querySelector("span[data-selected='no']");
+    console.log(selected);
+    console.log(unselected);
+    selected.style.color = "black";
+    unselected.style.color = "gray";
+}
 
