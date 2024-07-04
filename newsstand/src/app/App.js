f6f808047bff498ead2358cbe2618cb02d61c0d2
g_mi <62610032+akrudal@users.noreply.github.com>
2024-07-04 15:07:04 +0900

    REFACTOR: 함수형 컴포넌트를 다시 설계해요. (#7)
    * feature: remove function component element
    
    * refactor: function component

--- newsstand/src/app/App.js
+++ newsstand/src/app/App.js
@@ -1,33 +1,71 @@
-import { createElement } from './createElement.js';
-import { render } from './render.js';
+import AutoRollingNews from "../components/autoRollingNews/autoRollingNews.js";
+import NewsList from "../views/newsList.js";
 
-const App = () => {
-    return createElement('div', { class: 'content-container' },
-        createElement('div', { class: 'main-container' },
-            createElement('div', { class: 'title-container' },
-                createElement('div', {class: 'logo-container'},
-                    createElement('img', { src: '../../assets/icons/newspaper.svg', alt: 'newspaper icon' }),
-                    createElement('span', { class: 'logo-label'}, '뉴스스탠드')
-                ),
-                createElement('div', { class: 'day-label'}, '2023. 02. 10. 금요일')
-            ),
-            createElement('div', { class: 'headline-container' },
-                createElement('autoRollingNews', null),
-                createElement('autoRollingNews', null),
-            ),
-            createElement('div', { class: 'menu-container' },
-                createElement('div', { class: 'subscribe-menu-container'},
-                    createElement('button', { class: 'subscribe-menu-button'}, '전체 언론사'),
-                    createElement('button', { class: 'subscribe-menu-button'}, '내가 구독한 언론사')
-                ),
-                createElement('div', null,
-                    createElement('img', { src: '../../assets/icons/list-view.svg', alt: 'list view icon'}),
-                    createElement('img', { src: '../../assets/icons/grid-view.svg', alt: 'grid view icon'})
-                )
-            ),
-            createElement('newsList', null)
-        )
-    );
-};
 
-render(App(), document.getElementById('app'));
+// App 컴포넌트
+export const App = () => {
+    const appContainer = document.createElement('div');
+    appContainer.className = 'content-container';
+
+    const html = `
+        <div class="main-container">
+            <div class="title-container">
+                <div class="logo-container">
+                    <img src="../../assets/icons/newspaper.svg" alt="newspaper icon">
+                    <span class="logo-label">뉴스스탠드</span>
+                </div>
+                <div class="day-label">2023. 02. 10. 금요일</div>
+            </div>
+            <div class="headline-container"></div>
+            <div class="menu-container">
+                <div class="subscribe-menu-container">
+                    <button class="subscribe-menu-button">전체 언론사</button>
+                    <button class="subscribe-menu-button">내가 구독한 언론사</button>
+                </div>
+                <div>
+                    <img src="../../assets/icons/list-view.svg" alt="list view icon">
+                    <img src="../../assets/icons/grid-view.svg" alt="grid view icon">
+                </div>
+            </div>
+            <div class="newsList-container"></div>
+        </div>
+    `;
+
+
+    function makeHeadline(appContainer) {
+        const headlineContainer = appContainer.querySelector('.headline-container');
+    
+        const autoRollingNews1 = AutoRollingNews({
+            company: '연합뉴스',
+            title: '[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출'
+        });
+        const autoRollingNews2 = AutoRollingNews({
+            company: '중앙일보',
+            title: '[2보] 새로운 뉴스 타이틀'
+        });
+    
+        headlineContainer.appendChild(autoRollingNews1.element);
+        headlineContainer.appendChild(autoRollingNews2.element);
+    }
+
+    function makeNewsList(appContainer) {
+        const newsListContainer = appContainer.querySelector('.newsList-container');
+
+        const newsList = NewsList();
+
+        newsListContainer.appendChild(newsList.element);
+    }
+
+    function initializeApp() {
+        appContainer.innerHTML = html;
+
+        makeHeadline(appContainer);
+        makeNewsList(appContainer);
+
+        document.body.appendChild(appContainer);
+    }
+
+    initializeApp();
+}
+
+App()
--- newsstand/src/views/companyInfo.js
+++ newsstand/src/views/companyInfo.js
@@ -1,11 +1,11 @@
-import { createElement } from "../app/createElement.js";
+// import { createElement } from "../app/createElement.js";
 
-export const CompanyInfo = (props) => {
-    return createElement('div', null,
-        createElement('div', null, "서울경제"),
-        createElement('div', null, "2023.02.10. 18:27 편집"),
-        createElement('button', null, "구독하기")
-    );
-};
+// export const CompanyInfo = (props) => {
+//     return createElement('div', null,
+//         createElement('div', null, "서울경제"),
+//         createElement('div', null, "2023.02.10. 18:27 편집"),
+//         createElement('button', null, "구독하기")
+//     );
+// };
 
-export default CategoryButton;
+// export default CategoryButton;
