6565fda90a1e137a7020aa41bc550d564d854924
pccommen <pccommen@gmail.com>
2024-07-04 14:24:40 +0900

    fix/클래스 이름 및 css 구조변경

--- frontend/static/js/NewsstandTitle/index.js
+++ frontend/static/js/NewsstandTitle/index.js
@@ -2,7 +2,7 @@ import { getTimetoString } from "./getTimeToString.js";
 
 export function initNewsstandHeaderTime() {
     const currentDateString = getTimetoString("yyyy. MM. dd. DDD요일");
-    const currentDateDOM = document.querySelector('.titleDate');
+    const currentDateDOM = document.querySelector('#headerDate');
 
     currentDateDOM.textContent = currentDateString;
 }
