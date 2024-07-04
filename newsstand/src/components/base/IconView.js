f8669c1cc6a0accb30b9e829e6f880d6ea2fd379
admin <admin@adminui-noteubug.local>
2024-07-04 17:29:21 +0900

    Add: LEFT_BUTTON, RIGHT_BUTTON 을 Icon enum에 추가

--- newsstand/src/components/base/IconView.js
+++ newsstand/src/components/base/IconView.js
@@ -4,12 +4,14 @@ export const Icon = {
     X: './assets/x_ico.svg',
     LIST_CATEGORY: './assets/list_category_ico.svg',
     GRID_CATEGORY: './assets/grid_category_ico.svg',
+    LEFT_BUTTON: './assets/left_button_ico.svg',
+    RIGHT_BUTTON: './assets/right_button_ico.svg',
 }
 
 const IconView = (props) => {
     return {
         element: `
-        <img class="icon-view" src="${props.icon}" alt="icon"/>
+            <img class="icon-view" src="${props.icon}" alt="icon"/>
         `,
     }
 }
