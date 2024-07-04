37f9207a520ce708856b78e30545ed5c084ed3c6
guridaek <xylitol79@gmail.com>
2024-07-04 14:46:24 +0900

    feat: 속보 자동 롤링

--- src/components/AutoRollingNews/AutoRollingNews.js
+++ src/components/AutoRollingNews/AutoRollingNews.js
@@ -1,23 +1,91 @@
 import "./AutoRollingNews.css";
 
-function AutoRollingNews({ $target, position = "beforeend" }) {
+function AutoRollingNews({ $target, position = "beforeend", news }) {
   this.$element = document.createElement("div");
   this.$element.className = "viewer";
   $target.insertAdjacentElement(position, this.$element);
 
+  function getNewsTitleTemplate(str, idx) {
+    const classList = ["newsTitle", "ellipsis"];
+
+    if (idx === 0) classList.push("current");
+    else if (idx === 1) classList.push("next");
+    else if (idx === 2) classList.push("prev");
+
+    return `<li class="${classList.join(" ")}"><a>${str}</a></li>`;
+  }
+
+  function initializeBanner($banner) {
+    let interval = window.setInterval(() => {
+      rollingBanner($banner);
+    }, 5000);
+
+    $banner.addEventListener("mouseenter", () => {
+      window.clearInterval(interval);
+    });
+
+    $banner.addEventListener("mouseleave", () => {
+      interval = window.setInterval(() => {
+        rollingBanner($banner);
+      }, 5000);
+    });
+  }
+
+  function rollingBanner($banner) {
+    $banner.querySelector(".newsTitle.prev").classList.remove("prev");
+
+    let current = $banner.querySelector(".newsTitle.current");
+    current.classList.remove("current");
+    current.classList.add("prev");
+
+    let next = $banner.querySelector(".newsTitle.next");
+
+    if (next.nextElementSibling) {
+      next.nextElementSibling.classList.add("next");
+    } else {
+      $banner.querySelector("ul li:first-child").classList.add("next");
+    }
+
+    next.classList.remove("next");
+    next.classList.add("current");
+  }
+
   this.render = () => {
     this.$element.innerHTML = /* html */ `
-      <section class="LatestNews">
+      <section class="breakingNews">
         <p class="company">연합뉴스</p>
-        <p class="newsTitle">[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출</p>
+        <div id="leftBanner" class="banner">
+          <ul>
+            ${news
+              .slice(0, Math.floor(news.length / 2))
+              .map(getNewsTitleTemplate)
+              .join("\n")}
+          </ul>
+        </div>
       </section>
-      <section class="LatestNews">
+      <section class="breakingNews">
         <p class="company">연합뉴스</p>
-        <p class="newsTitle">[속보] 與최고위원 본경선, 김병민·김용태·김재원·민영삼</p>
+        <div id="rightBanner" class="banner">
+          <ul>
+            ${news
+              .slice(Math.floor(news.length / 2))
+              .map(getNewsTitleTemplate)
+              .join("\n")}
+          </ul>
+        </div>
       </section>`;
   };
 
   this.render();
+
+  const $leftBanner = this.$element.querySelector("#leftBanner");
+  const $rightBanner = this.$element.querySelector("#rightBanner");
+
+  initializeBanner($leftBanner);
+
+  setTimeout(() => {
+    initializeBanner($rightBanner);
+  }, 1000);
 }
 
 export default AutoRollingNews;
