ffe3f68a829b7cdf839c1b471f0f0c827fc862d3
pccommen <pccommen@gmail.com>
2024-07-04 18:45:25 +0900

    feat: 주석추가

--- frontend/static/js/mainContentControl/index.js
+++ frontend/static/js/mainContentControl/index.js
@@ -6,33 +6,47 @@ let currentArticleDetailIndex = 1;
 let recentIntervalId = 0;
 
 function initMainContent() {
+    // 다음 버튼과 이전버튼에 각각 다음 기사를 볼수있는 로직 추가
     const currentDom = document.querySelector("#mainContentArea");
     document.querySelector("#articlesNextButton").addEventListener('click', () => {
         const nextDom = document.querySelector("#nextMainContentArea");
         moveToNextPage("", currentDom, nextDom);
+        // 임시로 현재 index를 check 및 변경 함수
         currentArticleDetailIndex += 1;
+        // 새로운 기사를 확인할시 progress를 초기화해야함
         intervalRestart();
     });
     document.querySelector("#articlesPreviousButton").addEventListener('click', () => {
         const previousDom = document.querySelector("#previousMainContentArea");
         moveToPreviousPage("", currentDom, previousDom);
+        // 임시로 현재 index를 check 및 변경 함수
         currentArticleDetailIndex -= 1;
+        // 새로운 기사를 확인할시 progress를 초기화해야함
         intervalRestart();
     });
 
+    // 초기화로직에서 첫번쨰 nav Element가 선택될수 있도록하기위한 초기화 로직
     const navElements = document.querySelectorAll(".mainContentNavElement");
     navSelected(1, navElements[0]);
+    // 모든 navElement에 eventListner 추가
     navElements.forEach((element, index) => {
         element.addEventListener('click', () => {
+            // 모든 nav element 비활성화
             removeSelected(navElements);
+            // index 초기화
             currentArticleDetailIndex = 1;
+            // 현재 선택된 nav element 활성화
             navSelected(currentArticleDetailIndex, element);
+            // 현재 IT를 선택했고, 연애를 누른다면 오른쪽으로 기사가 넘어가고
+            // 현재 IT를 선택했고, 종합/경제를 누른다면 왼쪽으로 기사가 넘어가기 위한 분기처리
             if (index > currentArticleIndex) {
                 const nextDom = document.querySelector("#nextMainContentArea");
+                // 다음 페이지로 이동
                 moveToNextPage("", currentDom, nextDom);
             }
             else {
                 const previousDom = document.querySelector("#previousMainContentArea");
+                // 이전 페이지로 이동
                 moveToPreviousPage("", currentDom, previousDom);
             }
             currentArticleIndex = index;
@@ -40,14 +54,19 @@ function initMainContent() {
         });
     });
     recentIntervalId = setInterval(() => {
-        restartProgress();
+        // 다음 페이지
         goToNext();
+        // 현재활성화된 nav element에 있는 progress 초기화 및 재시작
+        restartProgress();
     }, 20000);
 }
 
 function intervalRestart() {
+    // progress 초기화 및 재시작
     restartProgress();
+    // 기존 interval 취소
     clearInterval(recentIntervalId);
+    // 새로운 interval 설정
     recentIntervalId = setInterval(() => {
         restartProgress();
         goToNext();
--- frontend/static/js/mainContentControl/pageControl.js
+++ frontend/static/js/mainContentControl/pageControl.js
@@ -1,19 +1,28 @@
 
 // data는 언론사와 그안의 기사들
 export function moveToNextPage(data, currentDom, nextDom) {
+    // 더미를 활용한 template literal
     const template = `<div id="mainContentTitle" class="Layout__row alignItems-center gap10"><img class="modeImg" src="./static/assets/images/grid-view.svg" alt="Description of SVG"><span>2023.02.10. 18:24편집</span><img class="modeImg" src="./static/assets/images/subscribeButton.svg" alt="Description of SVG"></div><div id="mainContentArticles" class="Layout__row"><div id="mainContentRepresentiveArticle" class="Layout__column-center"><img src="./static/assets/images/tempImage.svg" alt="Description of SVG"><span class="marginTop16">국민연금의 몽니…현대百 지주사 불발</span></div><div id="mainContentAritcleList"><ui><li>"위스키 사려고 이틀 전부터 줄 섰어요"</li><li>'방시혁 제국'이냐 '카카오 왕국'이냐…K엔터 누가 거머쥘까</li><li>사용후핵연료 저장시설 포화…이대론 7년 뒤 원전 멈춘다</li><li>[단독] 원희룡 "해외건설 근로자 소득공제 월 500만원으로 상향할 것"</li><li>태평양에는 우영우의 고래만 있는게 아니었다 [로비의 그림]</li><li>LG엔솔, 폴란드 자동차산업협회 가입…“유럽서 목소리 키운다”</li></ui><div>서울경제 언론사에서 직접 편집한 뉴스입니다.</div></div></div>`
 
+    // 비어져있는 다음 element에 주입
+    // dom이라는 변수명을 잘 안사용한다는걸 지금봤습니다. 내일 수정하겠습니다.
     nextDom.insertAdjacentHTML('beforeend', template);
+    // 애니메이션 시작을 위한 class추가
     nextDom.classList.add("animationStartNext");
     currentDom.classList.add("animationStartNext");
     setTimeout(() => {
+        // 애니메이션이 끝난 시점
+        // 현재 밀려나간 element에 존재하는 모든 child element 삭제
         while (currentDom.firstChild) {
             currentDom.removeChild(currentDom.firstChild);
         }
+        // 현재 밀려들어온 element에 존재하는 모든 child element 삭제
         while (nextDom.firstChild) {
             nextDom.removeChild(nextDom.firstChild);
         }
+        // 이번에 들어와야할 template literal 다시 주입
         currentDom.insertAdjacentHTML('beforeend', template);
+        // 애니메이션 종료로 element들을 원래 위치로 변경
         nextDom.classList.remove("animationStartNext");
         currentDom.classList.remove("animationStartNext");
     }, 1000);
