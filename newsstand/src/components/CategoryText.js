9331b468106bf826311232950ed34ccbe46f9045
admin <admin@adminui-noteubug.local>
2024-07-04 17:28:34 +0900

    feat: 선택되어있는 카테고리에 회사의 개수 및 현재 인덱스 표현

--- newsstand/src/components/CategoryText.js
+++ newsstand/src/components/CategoryText.js
@@ -1,7 +1,15 @@
 import useState from '../core/hooks/useState.js'
+import { mediaCategoryData } from '../datas/mockData.js'
+import { getCompanyCount } from '../datas/mockData.js'
+import { isIn } from '../utils/listUtils.js'
 
 const CategoryText = (props) => {
     const [isHover, setIsHover] = useState(false)
+
+    let companyCount = 0
+    if (isIn(props.text, mediaCategoryData)) {
+        companyCount = getCompanyCount(props.text)
+    }
 
     const handleMouseOver = () => {
         setIsHover(true)
@@ -13,6 +21,7 @@ const CategoryText = (props) => {
 
     const handleMouseClick = () => {
         props.setState(props.text)
+        props.setCurrentNewsId(1)
     }
 
     const bindEvents = () => {
@@ -24,9 +33,9 @@ const CategoryText = (props) => {
 
     return {
         element: `
-        <div class="category-text" id="category-text-${props.id}" style="font-weight: ${isHover || props.state === props.text ? 'bold' : 400}">
-            ${props.text}
-        </div>
+        <li class="category-text" id="category-text-${props.id}" style="font-weight: ${isHover || props.state === props.text ? 'bold' : 400}">
+            ${props.text} ${companyCount > 0 && props.state === props.text ? props.currentNewsId + '/' + companyCount : ''}
+        </li>
         `,
         bindEvents,
     }
