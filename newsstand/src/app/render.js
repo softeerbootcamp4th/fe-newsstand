90387342c2c5ef2a0146f06feae03501ae585967
akrudal <1mmgm.dev@gmail.com>
2024-07-04 12:25:32 +0900

    feature: remove function component element

--- newsstand/src/app/render.js
+++
@@ -1,7 +0,0 @@
-// render.js
-
-export const render = (component, container) => {
-    // container 요소를 비우고 새로운 컴포넌트를 렌더링합니다.
-    container.innerHTML = '';
-    container.appendChild(component);
-};
