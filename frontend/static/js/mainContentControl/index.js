42b9e6ac769da910736e17ee37fb779c504826dd
pccommen <pccommen@gmail.com>
2024-07-05 14:17:40 +0900

    feat: 데이터 추가에 따른 기존 Maincontent 로직 추가
    1. 기존에 한 카테고리의 끝에 도달하면 처음으로 돌아갔지만 이제는 다른 카테고리로 넘어감
    2. template literal에 기존 더미값으로 넣어놨던것은 실제 데이터로 치환

--- frontend/static/js/mainContentControl/index.js
+++ frontend/static/js/mainContentControl/index.js
@@ -1,56 +1,63 @@
+import { NEWS } from "../../data/data.js";
 import { navSelected, removeSelected } from "./navControl.js";
 import { moveToNextPage, moveToPreviousPage } from "./pageControl.js";
 
+const categories = ["종합/경제", "방송/통신", "IT", "스포츠/연애", "매거진/전문지", "지역"];
+const NEWS_Sorted = categories.map((category) => {
+    return NEWS.filter((element) => element.category == category);
+});
 let currentArticleIndex = 0;
-let currentArticleDetailIndex = 1;
+let currentArticleDetailIndex = 0;
 let recentIntervalId = 0;
 
 function initMainContent() {
-    // 다음 버튼과 이전버튼에 각각 다음 기사를 볼수있는 로직 추가
+    // 최초 초기화 로직
+    const currentData = NEWS_Sorted[currentArticleIndex][currentArticleDetailIndex];
+    const template = `<div id="mainContentTitle" class="Layout__row alignItems-center gap10"><img src="${currentData.mediaCompanyImageURL}" style="width: 52.5px; height:20px;" alt="img"><span>${currentData.lastDate}편집</span><img class="modeImg" src="./static/assets/images/subscribeButton.svg" alt="Description of SVG"></div><div id="mainContentArticles" class="Layout__row"><div id="mainContentRepresentiveArticle" class="Layout__column-center"><img src="${currentData.articles[0].imageURL}" alt="Description of SVG" style="width: 320px; height: 200px"><span class="marginTop16">${currentData.articles[0].articleTitle}</span></div><div id="mainContentAritcleList"><ui><li>${currentData.articles[1].articleTitle}</li><li>${currentData.articles[2].articleTitle}</li><li>${currentData.articles[3].articleTitle}</li><li>${currentData.articles[4].articleTitle}</li><li>${currentData.articles[5].articleTitle}</li><li>${currentData.articles[6].articleTitle}</li></ui><div>${currentData.name} 언론사에서 직접 편집한 뉴스입니다.</div></div></div>`
     const currentDom = document.querySelector("#mainContentArea");
+    currentDom.insertAdjacentHTML('beforeend', template);
+    // 다음 버튼과 이전버튼에 각각 다음 기사를 볼수있는 로직 추가
     document.querySelector("#articlesNextButton").addEventListener('click', () => {
         const nextDom = document.querySelector("#nextMainContentArea");
-        moveToNextPage("", currentDom, nextDom);
-        // 임시로 현재 index를 check 및 변경 함수
-        currentArticleDetailIndex += 1;
-        // 새로운 기사를 확인할시 progress를 초기화해야함
+        addIndex();
+        moveToNextPage(NEWS_Sorted[currentArticleIndex], currentDom, nextDom, currentArticleDetailIndex);
+
         intervalRestart();
     });
     document.querySelector("#articlesPreviousButton").addEventListener('click', () => {
         const previousDom = document.querySelector("#previousMainContentArea");
-        moveToPreviousPage("", currentDom, previousDom);
-        // 임시로 현재 index를 check 및 변경 함수
-        currentArticleDetailIndex -= 1;
-        // 새로운 기사를 확인할시 progress를 초기화해야함
+        subtractIndex();
+        moveToPreviousPage(NEWS_Sorted[currentArticleIndex], currentDom, previousDom, currentArticleDetailIndex);
         intervalRestart();
     });
 
     // 초기화로직에서 첫번쨰 nav Element가 선택될수 있도록하기위한 초기화 로직
     const navElements = document.querySelectorAll(".mainContentNavElement");
-    navSelected(1, navElements[0]);
+    navSelected(0, NEWS_Sorted[currentArticleIndex].length, navElements[0]);
     // 모든 navElement에 eventListner 추가
     navElements.forEach((element, index) => {
         element.addEventListener('click', () => {
             // 모든 nav element 비활성화
             removeSelected(navElements);
             // index 초기화
-            currentArticleDetailIndex = 1;
+            const beforeIndex = currentArticleIndex;
+            currentArticleIndex = index;
+            currentArticleDetailIndex = 0;
             // 현재 선택된 nav element 활성화
-            navSelected(currentArticleDetailIndex, element);
+            console.log(NEWS_Sorted[currentArticleIndex]);
+            navSelected(currentArticleDetailIndex, NEWS_Sorted[currentArticleIndex].length, element);
             // 현재 IT를 선택했고, 연애를 누른다면 오른쪽으로 기사가 넘어가고
             // 현재 IT를 선택했고, 종합/경제를 누른다면 왼쪽으로 기사가 넘어가기 위한 분기처리
-            if (index > currentArticleIndex) {
+            if (index > beforeIndex) {
                 const nextDom = document.querySelector("#nextMainContentArea");
                 // 다음 페이지로 이동
-                moveToNextPage("", currentDom, nextDom);
+                moveToNextPage(NEWS_Sorted[currentArticleIndex], currentDom, nextDom, 0);
             }
             else {
                 const previousDom = document.querySelector("#previousMainContentArea");
                 // 이전 페이지로 이동
-                moveToPreviousPage("", currentDom, previousDom);
+                moveToPreviousPage(NEWS_Sorted[currentArticleIndex], currentDom, previousDom, 0);
             }
-            currentArticleIndex = index;
-
         });
     });
     recentIntervalId = setInterval(() => {
@@ -77,14 +84,14 @@ function restartProgress() {
     const element = getSelectedNav();
     const navElements = getAllNavElements();
     removeSelected(navElements);
-    navSelected(currentArticleDetailIndex, element);
+    navSelected(currentArticleDetailIndex, NEWS_Sorted[currentArticleIndex].length, element);
 }
 
 function goToNext() {
-    currentArticleDetailIndex += 1;
+    addIndex();
     const currentDom = document.querySelector("#mainContentArea");
     const nextDom = document.querySelector("#nextMainContentArea");
-    moveToNextPage("", currentDom, nextDom);
+    moveToNextPage(NEWS_Sorted[currentArticleIndex], currentDom, nextDom, currentArticleDetailIndex);
 }
 
 function getSelectedNav() {
@@ -95,6 +102,34 @@ function getAllNavElements() {
     return document.querySelectorAll(".mainContentNavElement");
 }
 
+function addIndex() {
+    var currentLenth = NEWS_Sorted[currentArticleIndex].length;
+    if (currentArticleDetailIndex == currentLenth - 1) {
+        currentArticleIndex = (currentArticleIndex + 1) % NEWS_Sorted.length;
+        currentArticleDetailIndex = 0;
+        const navElements = getAllNavElements();
+        const element = navElements[currentArticleIndex];
+        removeSelected(navElements);
+        navSelected(currentArticleDetailIndex, NEWS_Sorted[currentArticleIndex].length, element);
+    }
+    else {
+        currentArticleDetailIndex += 1;
+    }
+}
+
+function subtractIndex() {
+    if (currentArticleDetailIndex == 0) {
+        currentArticleIndex = (currentArticleIndex + NEWS_Sorted.length - 1) % NEWS_Sorted.length;
+        currentArticleDetailIndex = 0;
+        const navElements = getAllNavElements();
+        const element = navElements[currentArticleIndex];
+        removeSelected(navElements);
+        navSelected(currentArticleDetailIndex, NEWS_Sorted[currentArticleIndex].length, element); restartProgress();
+    }
+    else {
+        currentArticleDetailIndex -= 1;
+    }
+}
 
 
 initMainContent();
