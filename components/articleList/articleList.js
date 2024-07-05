ffca07296581783ed18aef8da203a1478cd8285c
JSK0406 <12180543@inha.edu>
2024-07-05 14:11:51 +0900

    [feat/3] refactor: articleList 함수 분리, 에러 처리 추가, Event Delegation 적용
    - 함수 분리
    - json load 실패 시 처리하는 try catch 구문 추가
    - 버튼 모두에 event를 부여하는 대신 상위 wrapper에 부여하여 버블링을 통해 성능 개선
    - 코드 정리

--- components/articleList/articleList.js
+++ components/articleList/articleList.js
@@ -1,228 +1,195 @@
-// 초기 상태
+// 상수 정의
+const DATA_SRC = '/datas/menuInfo.json';
+const MEDIA_LIST = ['데일리안', '서울경제', '세계일보', '스포츠동아', '스포츠서울', '아시아경제', '이데일리', '조선일보', '파이낸셜뉴스', '헤럴드경제'];  // 썸네일에 임의의 이미지 경로를 넣기 위해
+const CATEGORY_TIMEOUT = 1000 * 20;
 
+// 전역 변수 선언
+let menuInfo;
+let menuCurrentPage = 1;
+let menuLastPage = 0;
+let nowMenuIdx = 0;
+let categoryTimeoutId;
+
+// 초기화 함수
+const init = async () => {
+    try {
+        menuInfo = await loadJsonData(DATA_SRC);
+        menuInfo = menuInfo.data;
+        const articleArea = document.querySelector('#article-list-area');
+        renderInitialUI(menuInfo, articleArea);
+        addEventListeners();
+
+        // 첫번째 카테고리 버튼에 class 부여
+        setDataWithTab(document.querySelectorAll('.menu-btn-wrapper')[0], 0);
+    } catch (error) {
+        alert('fail');
+    }
+}
+
+// JSON 데이터 로드 함수
 const loadJsonData = async (src) => {
     const res = await fetch(src);
+    if (!res.ok) {
+        throw new Error(`Failed to fetch data from ${src}`);
+    }
     return res.json();
 }
 
-let menuInfo = await loadJsonData('/datas/menuInfo.json')
-menuInfo = menuInfo.data
+// 초기 UI 렌더링 함수
+const renderInitialUI = (menuInfo, articleArea) => {
+    articleArea.innerHTML = `
+        <div id="articleList-wrapper">
+            <div id="article-header-wrapper" class="flex-row-between">
+                <div id="media-wrapper" class="inline-tag">
+                    <button class="btn mode-selection-btn mode-selection-btn-clicked">전체 언론사</button>
+                    <button class="btn mode-selection-btn">내가 구독한 언론사</button>
+                </div>
+                <div id="icon-wrapper" class="inline-tag flex-row-between">
+                    <button class="btn view-btn view-btn-clicked">
+                        <img src="/icons/list-view.png" alt="" width="24px" height="24px">
+                    </button>
+                    <button class="btn view-btn">
+                        <img src="/icons/grid-view.png" alt="" width="24px" height="24px">
+                    </button>
+                </div>
+            </div>
+            <div id="article-menu-wrapper">
+                ${createMenuList(menuInfo)}
+            </div>
+            <div id="article-wrapper">
+                <div id="article-selection-wrapper"></div>
+                <div id="article-content-wrapper">
+                    <div id="content-header-wrapper" class="flex-row">
+                        <img id="media-img" src="" alt="">
+                        <h4 id="updated-date-tag" style="font-size: 12px; font-weight: 400;"> 편집</h4>
+                        <button id="subscribe-btn" class="btn">+ 구독하기</button>
+                    </div>
+                    <div id="content-body-wrapper" class="flex-row-between">
+                        <aside id="thumbnail-part">
+                            <img id="thumbnail-img" src="" alt="" width="320px" height="200px">
+                            <p id="thumbnail-detail">ABC</p>
+                        </aside>
+                        <ul id="article-li-part" class="flex-col-between">
+                            <p id="li-part-info"></p>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
-const articleArea = document.querySelector('#article-list-area');
-
-let menuCurrentPage = 1;
-let menuLastpage = 0;
-let nowMenuIdx = 0;
-
-// create 태그
+// 메뉴 리스트 생성 함수
 const createMenuList = (menuInfo) => {
-    return menuInfo.map((info) => (
-        {
-            menuName: info.category,
-            menuLastPage: info.totalPages
-        }
-    )).map((info, idx) => `
+    return menuInfo.map((info, idx) => `
         <div class="menu-btn-wrapper">
             <button class="flex-row-between article-menu-btn ${idx === 0 ? "article-menu-btn-clicked" : ""}">
-                <h5>
-                    ${info.menuName}
-                </h5>
-                <h5 class="article-menu-pages display-none">
-                    ${1} / ${info.menuLastPage}
-                </h5>
+                <h5>${info.category}</h5>
+                <h5 class="article-menu-pages display-none">1 / ${info.totalPages}</h5>
             </button>
-            <div class="fill-background">
-            </div>
+            <div class="fill-background"></div>
         </div>
     `).join('');
 }
 
-const createArticleLiPart = (articleData) => 
-    {return articleData.map((articleItem) => `
-        <li>${articleItem}</li>
-    `).join('')}
+// 기사 리스트 생성 함수
+const createArticleLiPart = (articleData) => {
+    return articleData.map((articleItem) => `<li>${articleItem}</li>`).join('');
+}
 
-articleArea.innerHTML = `
-    <div id="articleList-wrapper">
-        <div id="article-header-wrapper" class="flex-row-between">
-            <div id="media-wrapper inline-tag">
-                <button class="btn mode-selection-btn mode-selection-btn-clicked">전체 언론사</button>
-                <button class="btn mode-selection-btn">내가 구독한 언론사</button>
-            </div>
-            <div id="icon-wrapper inline-tag flex-row-between">
-                <button class="btn view-btn view-btn-clicked">
-                    <img src="/icons/list-view.png" alt="" width="24px" height="24px">
-                </button>
-                <button class="btn view-btn">
-                    <img src="/icons/grid-view.png" alt="" width="24px" height="24px">
-                </button>
-            </div>
-        </div>
-        <div id="article-menu-wrapper">
-            ${createMenuList(menuInfo)}
-        </div>
-        <div id="article-wrapper">
-            <div id="article-selection-wrapper">
-            </div>
-            <div id="article-content-wrapper">
-                <div id="content-header-wrapper" class="flex-row">
-                    <img id="media-img" src="" alt="">
-                    <h4 id="updated-date-tag" style="font-size: 12px; font-weight: 400;"> 편집</h4>
-                    <button id="subscribe-btn" class="btn">+ 구독하기</button>
-                    </div>
-                    <div id="content-body-wrapper" class="flex-row-between">
-                    <aside id="thumbnail-part">
-                        <img id="thumbnail-img" src="" alt="" width="320px" height="200px">
-                        <p id="thumbnail-detail">ABC</p>
-                    </aside>
-                    <ul id="article-li-part" class="flex-col-between">
-                        <p id="li-part-info"></p>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-`
-
-// mode-selection-event
-const modeSelectionBtns = document.querySelectorAll('.mode-selection-btn');
-modeSelectionBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-        modeSelectionBtns.forEach(b => b.classList.remove('mode-selection-btn-clicked'));
-        this.classList.add('mode-selection-btn-clicked');
+// 이벤트 리스너 추가 함수
+const addEventListeners = () => {
+    // Mode selection event
+    document.querySelector('#media-wrapper').addEventListener('click', (event) => {
+        if (event.target.classList.contains('mode-selection-btn')) {
+            document.querySelectorAll('.mode-selection-btn').forEach(btn => btn.classList.remove('mode-selection-btn-clicked'));
+            event.target.classList.add('mode-selection-btn-clicked');
+        }
     });
-});
 
-// view-selection-event
-const viewBtns = document.querySelectorAll('.view-btn');
-viewBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-        viewBtns.forEach(b => b.classList.remove('view-btn-clicked'));
-        this.classList.add('view-btn-clicked');
+    // View selection event
+    document.querySelector('#icon-wrapper').addEventListener('click', (event) => {
+        if (event.target.closest('.view-btn')) {
+            document.querySelectorAll('.view-btn').forEach(btn => btn.classList.remove('view-btn-clicked'));
+            event.target.closest('.view-btn').classList.add('view-btn-clicked');
+        }
     });
-});
 
-// category-selection-event
-let categoryTimeoutId; // setTimeoutId
+    // Category selection event
+    document.querySelector('#article-menu-wrapper').addEventListener('click', (event) => {
+        if (event.target.closest('.menu-btn-wrapper')) {
+            const btnWrapper = event.target.closest('.menu-btn-wrapper');
+            const idx = Array.from(btnWrapper.parentNode.children).indexOf(btnWrapper);
+            setDataWithTab(btnWrapper, idx);
+        }
+    });
+}
 
-const mediaList = ['데일리안', '서울경제', '세계일보', '스포츠동아', '스포츠서울', '아시아경제', '이데일리', '조선일보', '파이낸셜뉴스', '헤럴드경제'];
-
-const insertContent = (nowMenuIdx, menuCurrentPage, menuLastpage) => {
-    const nowInfo = menuInfo[nowMenuIdx].thumbnailDatas[menuCurrentPage-1]
+// 콘텐츠 삽입 함수
+const insertContent = (nowMenuIdx, menuCurrentPage, menuLastPage) => {
+    const nowInfo = menuInfo[nowMenuIdx].thumbnailDatas[menuCurrentPage - 1];
     
-    // 페이지 정보
-    document.querySelector('.article-menu-pages').innerText = `${menuCurrentPage} / ${menuLastpage}`
-
-    // 미디어 사진
-    document.querySelector('#media-img').src = `/images/logos/${nowInfo.thumbnailMediaName}.png`
-    
-    // 수정일
-    document.querySelector('#updated-date-tag').innerText = `${nowInfo.thumbnailUpdatedDate} 편집`
-    
-    // 구독하기에 해당 신문사 정보 삽입
-    
-    // 썸네일 경로
-    document.querySelector('#thumbnail-img').src = `/images/logos/${mediaList[menuCurrentPage]}.png`
-    
-    // 썸네일 디테일
+    document.querySelector('.article-menu-pages').innerText = `${menuCurrentPage} / ${menuLastPage}`;
+    document.querySelector('#media-img').src = `/images/logos/${nowInfo.thumbnailMediaName}.png`;
+    document.querySelector('#updated-date-tag').innerText = `${nowInfo.thumbnailUpdatedDate} 편집`;
+    document.querySelector('#thumbnail-img').src = `/images/logos/${MEDIA_LIST[menuCurrentPage]}.png`;
     document.querySelector('#thumbnail-detail').innerText = `${nowInfo.thumbnailDetail}`;
-
-    // 목록 리스트, 안내 문구
     document.querySelector('#article-li-part').innerHTML = `
         ${createArticleLiPart(nowInfo.articleList)}
         <p id="li-part-info">${nowInfo.thumbnailMediaName}에서 직접 편집한 뉴스입니다.</p>
-    `
+    `;
 }
 
+// 페이지 이동 함수
 const moveToNextPage = (thisBtn) => {
     const nextBtn = thisBtn.nextElementSibling !== null ? thisBtn.nextElementSibling : thisBtn.parentElement.firstElementChild;
     const totalMenuLength = menuInfo.length;
-    menuLastpage = menuInfo[nowMenuIdx].totalPages
-    categoryTimeoutId = setTimeout(() => { // 타이머 식별자 저장
-        // 다음 메뉴로 넘어감
-        if (menuCurrentPage === menuLastpage) {
-            // 페이지 세팅
+    menuLastPage = menuInfo[nowMenuIdx].totalPages;
+    
+    categoryTimeoutId = setTimeout(() => {
+        if (menuCurrentPage === menuLastPage) {
             menuCurrentPage = 1;
             nowMenuIdx += 1;
             if (nowMenuIdx === totalMenuLength) {
                 nowMenuIdx = 0;
             }
-            
-            // 다음 버튼으로 이동
-            thisBtn.classList.remove('menu-btn-wrapper-clicked')
-            nextBtn.classList.add('menu-btn-wrapper-clicked')
-            
-            moveToNextPage(nextBtn)
-        // 다음 페이지로 넘어감
+            thisBtn.classList.remove('menu-btn-wrapper-clicked');
+            nextBtn.classList.add('menu-btn-wrapper-clicked');
+            moveToNextPage(nextBtn);
         } else {
-            // 페이지 세팅
             menuCurrentPage += 1;
-            thisBtn.firstElementChild.children[1].innerText = `${menuCurrentPage} / ${menuLastpage}`   
-            thisBtn.children[1].remove();
-
-            // fillBackground를 지웠다가 다시 생성
+            thisBtn.querySelector('.article-menu-pages').innerText = `${menuCurrentPage} / ${menuLastPage}`;
+            thisBtn.querySelector('.fill-background').remove();
+            
             const fillBackground = document.createElement('div');
             fillBackground.classList.add('fill-background');
             thisBtn.appendChild(fillBackground);
-
-            moveToNextPage(thisBtn)
+            
+            moveToNextPage(thisBtn);
         }
-        insertContent(nowMenuIdx, menuCurrentPage, menuLastpage)
-    }, 1000 * 20)
+        insertContent(nowMenuIdx, menuCurrentPage, menuLastPage);
+    }, CATEGORY_TIMEOUT);
 }
 
-const articleMenuWrapper = document.querySelectorAll('.menu-btn-wrapper');
+// 탭 데이터 설정 함수
 const setDataWithTab = (btnWrapper, idx) => {
     nowMenuIdx = idx;
     menuCurrentPage = 1;
-    menuLastpage = menuInfo[nowMenuIdx].totalPages
-    
-    // 클릭 시 초기 정보 삽입
-    insertContent(nowMenuIdx, menuCurrentPage, menuLastpage)
-    
-    // 클릭 시 모든 탭들의 페이지 정보 초기화
-    document.querySelectorAll('.menu-btn-wrapper').forEach((btnWrapper, idx) => {
-        btnWrapper.children[0].children[1].innerText = `${menuCurrentPage} / ${menuInfo[idx].totalPages}`
-    })
+    menuLastPage = menuInfo[nowMenuIdx].totalPages;
 
-    // timeout 초기화
+    insertContent(nowMenuIdx, menuCurrentPage, menuLastPage);
+
+    document.querySelectorAll('.menu-btn-wrapper').forEach((btnWrapper, idx) => {
+        btnWrapper.querySelector('.article-menu-pages').innerText = `${menuCurrentPage} / ${menuInfo[idx].totalPages}`;
+    });
+
     clearTimeout(categoryTimeoutId);
-    
-    // 클릭한 버튼에만 클래스 부여
-    const articleMenuWrapper = document.querySelectorAll('.menu-btn-wrapper');
-    articleMenuWrapper.forEach(b => b.classList.remove('menu-btn-wrapper-clicked'));
+
+    document.querySelectorAll('.menu-btn-wrapper').forEach(b => b.classList.remove('menu-btn-wrapper-clicked'));
     btnWrapper.classList.add('menu-btn-wrapper-clicked');
 
-    moveToNextPage(btnWrapper)
+    moveToNextPage(btnWrapper);
 }
 
-articleMenuWrapper.forEach((btnWrapper, idx) => {
-    btnWrapper.addEventListener('click', function() {
-        // 초기 페이지 세팅
-
-        nowMenuIdx = idx;
-        menuCurrentPage = 1;
-        menuLastpage = menuInfo[nowMenuIdx].totalPages
-        
-        // 클릭 시 초기 정보 삽입
-        btnWrapper.children[0].children[1].innerText = `1 / ${menuLastpage}`
-        insertContent(nowMenuIdx, menuCurrentPage, menuLastpage)
-        
-        // 클릭 시 모든 탭들의 페이지 정보 초기화
-        articleMenuWrapper.forEach((btnWrapper, idx) => {
-            btnWrapper.children[0].children[1].innerText = `${menuCurrentPage} / ${menuInfo[idx].totalPages}`
-        })
-        
-        // timeout 초기화
-        clearTimeout(categoryTimeoutId);
-        
-        // 클릭한 버튼에만 클래스 부여
-        articleMenuWrapper.forEach(b => b.classList.remove('menu-btn-wrapper-clicked'));
-        this.classList.add('menu-btn-wrapper-clicked');
-        
-        moveToNextPage(btnWrapper)
-    });
-});
-
-// 렌더링 시 초기 상태
-setDataWithTab(document.querySelectorAll('.menu-btn-wrapper')[0], 0)
+// 초기화 호출
+init();
