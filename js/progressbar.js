555be040d379e4d721edb5cbefb561fe8e5c79b6
cm2hcm2h <cm2hcm2h@naver.com>
2024-07-05 18:06:28 +0900

    feat: 클릭된 nav 아이템에 프로그래스바 애니메이션 구현

--- js/progressbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navItem = document.getElementById("nav-item-1");
-    
-    // 0.1초 지연 후 애니메이션 시작
-    setTimeout(() => {
-      navItem.classList.add('progress');
-    }, 100); // DOM이 로드된 후 애니메이션 시작
-  });
-  
