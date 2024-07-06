daa84fe907d1bcbedd8fc86a05b21ba536cc6689
DaeWon9 <qkreodnjs97@naver.com>
2024-07-06 23:28:35 +0900

    feat: viewType을 지정하는 기능 추가 list / grid

--- newsstand/src/layouts/source-selector/NewsSourceSelector.js
+++ newsstand/src/layouts/source-selector/NewsSourceSelector.js
@@ -1,27 +1,33 @@
-import useState from '../../core/hooks/useState.js'
 import createComponent from '../../core/component/component.js'
 import IconView, { Icon } from '../../components/base/IconView.js'
 import CategoryText from '../../components/category/CategoryText.js'
 import { generateRandomId } from '../../utils/idGenerator.js'
 
-const NewsSourceSelector = () => {
-    const [selectedSource, setSelectedSource] = useState('전체 언론사')
-
+const NewsSourceSelector = (props) => {
     const wholeCategory = createComponent(CategoryText, {
         id: generateRandomId(10),
         text: '전체 언론사',
-        state: selectedSource,
-        setState: setSelectedSource,
+        state: props.selectedSource,
+        onClick: () => props.setSelectedSource('전체 언론사'),
     })
     const subscribeCategory = createComponent(CategoryText, {
         id: generateRandomId(10),
         text: '내가 구독한 언론사',
-        state: selectedSource,
-        setState: setSelectedSource,
+        state: props.selectedSource,
+        onClick: () => props.setSelectedSource('내가 구독한 언론사'),
     })
 
-    const listCategoryButton = createComponent(IconView, { id: generateRandomId(10), icon: Icon.LIST_CATEGORY })
-    const gridCategoryButton = createComponent(IconView, { id: generateRandomId(10), icon: Icon.GRID_CATEGORY })
+    const listCategoryButton = createComponent(IconView, {
+        id: generateRandomId(10),
+        icon: Icon.LIST_CATEGORY,
+        onClick: () => props.setViewType('list'),
+    })
+
+    const gridCategoryButton = createComponent(IconView, {
+        id: generateRandomId(10),
+        icon: Icon.GRID_CATEGORY,
+        onClick: () => props.setViewType('grid'),
+    })
 
     return {
         element: `
