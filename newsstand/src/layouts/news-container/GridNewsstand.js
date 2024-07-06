22cda810a851c8aa7ec5bdae071e1c19b2d679ff
DaeWon9 <qkreodnjs97@naver.com>
2024-07-06 23:33:38 +0900

    feat: GridNewsstand에 전체 회사 데이터 바인딩

--- newsstand/src/layouts/news-container/GridNewsstand.js
+++ newsstand/src/layouts/news-container/GridNewsstand.js
@@ -1,30 +1,65 @@
-import NewsBox from './components/newsBox.js'
-import Button from '../../components/base/Button.js'
-import { mockData } from '../../datas/mockData.js'
+import NewsBox from '../../components/news/NewsBox.js'
+import createComponent from '../../core/component/component.js'
+import IconView, { Icon } from '../../components/base/IconView.js'
+import { getCompanyDataByPage, getMaxPage } from '../../datas/companyData.js'
+import { generateRandomId } from '../../utils/idGenerator.js'
+import useState from '../../core/hooks/useState.js'
 
 const GridNewsstand = () => {
-    const newsBoxes = mockData.map((news, index) => {
-        const newsBoxComponent = createComponent(NewsBox, { id: index + 1, icon: news.icon })
-        return newsBoxComponent
+    const [page, setPage] = useState(1)
+    const maxPage = getMaxPage()
+    let companyData = getCompanyDataByPage(page)
+
+    const newsBoxes = companyData.map((news) => {
+        return createComponent(NewsBox, {
+            id: news.id,
+            name: news.name,
+            icon: news.logo,
+            style: 'width: 100%; height:100%;',
+        })
     })
-    const buttonComponent = createComponent(Button, {
+
+    const leftButtonIcon = createComponent(IconView, {
         id: generateRandomId(10),
-        text: '구독하기',
-        style: 'width: 80px; height:10px;',
-        variant: 'white',
+        icon: Icon.LEFT_BUTTON,
     })
-    const buttonComponent2 = createComponent(Button, {
+
+    const rightButtonIcon = createComponent(IconView, {
         id: generateRandomId(10),
-        text: '해지하기',
-        style: 'width: 80px; height:10px;',
-        variant: 'gray',
+        icon: Icon.RIGHT_BUTTON,
     })
+
+    const handleRightButtonClick = () => {
+        if (page < maxPage) setPage(page + 1)
+    }
+
+    const handleLeftButtonClick = () => {
+        if (page > 1) setPage(page - 1)
+    }
+
+    const bindEvents = () => {
+        const leftBtn = document.querySelector('.left-btn')
+        const rightBtn = document.querySelector('.right-btn')
+
+        leftBtn.addEventListener('click', handleLeftButtonClick)
+        rightBtn.addEventListener('click', handleRightButtonClick)
+    }
 
     return {
         element: `
-        <div class="grid-news-container">
+        <div class="carousel-container">
+            <button class="carousel-btn left-btn">
+                ${page > 1 ? leftButtonIcon.element : ''}
+            </button>
+            <div class="grid-news-container">
+                ${newsBoxes.map((box) => box.element).join('')}
+            </div>
+            <button class="carousel-btn right-btn">
+                ${page < maxPage ? rightButtonIcon.element : ''}
+            </button>
         </div>
         `,
+        bindEvents,
     }
 }
 
