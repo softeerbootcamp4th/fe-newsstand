1aee2c2e6df1356b7517cc3172205ad98ea4c362
darkdulgi <daniel99285@gmail.com>
2024-07-06 15:32:32 +0900

    refactor : 롤링바에서  setInterval, setTimeout 제거, requestAnimationFrame 사용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
+import rollingNewsSection from "./rolling.js";
+
 const $viewDateDOM = document.querySelector(".date");
-const $newsPreviewDOMList = document.querySelectorAll(".newsPreview");
-const keyframes = [
-  { transform: "translateY(0%)" },
-  { transform: "translateY(-110%)" },
-];
-const options = {
-  duration: 500,
-};
-let mouseHover = [];
 
 const getDate = () => {
   const time = new Date();
@@ -22,56 +15,5 @@ const getDate = () => {
   $viewDateDOM.innerText = `${year}. ${month}. ${date} ${week[day]}요일`;
 };
 
-const loadNews = async () => {
-  let news = [];
-  let { ctg } = await (await fetch("/data/news.json")).json();
-  Object.keys(ctg).forEach((key) => {
-    news = [...news, ...ctg[key]];
-  });
-  console.log(Object.keys((ctg)));
-  let leftNewsIndex = 0;
-  let rightNewsIndex = 123456789; // 랜덤수
-  let newsLength = news.length;
-
-  const insertNews = (newsIndex, DOMIndex, first) => {
-    if (!first) {
-      $newsPreviewDOMList[DOMIndex].animate(keyframes, options);
-      $newsPreviewDOMList[DOMIndex + 1].animate(keyframes, options);
-    }
-    setTimeout(() => {
-      $newsPreviewDOMList[DOMIndex].innerText = news[newsIndex].com + '\n' + news[(newsIndex + 1) % newsLength].com;
-
-      $newsPreviewDOMList[DOMIndex + 1].innerText = news[newsIndex].title + '\n' + news[(newsIndex + 1) % newsLength].title;
-    }, 500);
-  }
-
-  insertNews(leftNewsIndex++, 0, 1);
-  insertNews(rightNewsIndex++ % newsLength, 2, 1);
-  setInterval(() => {
-    if (!mouseHover[0] && !mouseHover[1]) {
-      insertNews(leftNewsIndex % newsLength, 0, 0);
-      leftNewsIndex++;
-    }
-
-    if (!mouseHover[2] && !mouseHover[3]) {
-      setTimeout(() => {
-        if (!mouseHover[2] && !mouseHover[3]) {
-          insertNews(rightNewsIndex % newsLength, 2, 0);
-          rightNewsIndex++;
-        }
-      }, 1000);
-    }
-  }, 5000);
-}
-
-for (let i = 0; i < $newsPreviewDOMList.length; i++) {
-  $newsPreviewDOMList[i].addEventListener("mouseenter", () => {
-    mouseHover[i] = 1;
-  });
-  $newsPreviewDOMList[i].addEventListener("mouseleave", () => {
-    mouseHover[i] = 0;
-  });
-}
-
 getDate();
-loadNews();
+rollingNewsSection();
