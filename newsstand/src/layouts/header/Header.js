1a2d08948b74b964710b8d81f37ec4b3eb768c6f
admin <admin@adminui-noteubug.local>
2024-07-06 19:49:27 +0900

    refactor: getTodayString function 분리

--- newsstand/src/layouts/header/Header.js
+++ newsstand/src/layouts/header/Header.js
@@ -1,11 +1,7 @@
-const Header = () => {
-    const date = new Date()
-    const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
+import { getTodayString } from '../../utils/dateUtils.js'
 
-    const year = date.getFullYear()
-    const month = (date.getMonth() + 1).toString().padStart(2, '0')
-    const day = date.getDate().toString().padStart(2, '0')
-    const dayName = days[date.getDay()]
+const Header = () => {
+    let todayString = getTodayString()
 
     return {
         element: `
@@ -14,7 +10,7 @@ const Header = () => {
                 <img src='./assets/newsstand_ico.svg' style="height: 24px; margin-right: 8px;"/>
                 <span style="font-size: 18px;">뉴스스탠드</span>
             </div>
-            <div style="font-size: 16px; color: gray;">${year}.${month}.${day} ${dayName}</div>
+            <div style="font-size: 16px; color: gray;">${todayString}</div>
         </div>
         `,
     }
