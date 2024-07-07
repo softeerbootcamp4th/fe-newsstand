8edb2cb41b81e4f28350ad40330c91c74958ce26
DaeWon9 <qkreodnjs97@naver.com>
2024-07-07 13:40:21 +0900

    fix: props로 넘겨받은 데이터 바인딩 미스매치 수정

--- newsstand/src/components/news/NewsBox.js
+++ newsstand/src/components/news/NewsBox.js
@@ -3,44 +3,44 @@ import Button, { ButtonVariantProps } from '../base/Button.js'
 import { generateRandomId } from '../../utils/idGenerator.js'
 import createComponent from '../../core/component/component.js'
 import IconView, { Icon } from '../base/IconView.js'
-import { isSubscribed } from '../../utils/subscribeUtils.js'
+import { isSubscribed, handleSubscription } from '../../utils/subscribeUtils.js'
 
 const NewsBox = (props) => {
     const [isHover, setIsHover] = useState(false)
 
-    const handleMouseOver = () => {
+    const handleMouseEnter = () => {
         setIsHover(true)
     }
 
-    const handleMouseOut = () => {
+    const handleMouseLeave = () => {
         setIsHover(false)
     }
 
     const bindEvents = () => {
-        const newsBox = document.getElementById(`news-box-${props.id}`)
-        newsBox.addEventListener('mouseover', handleMouseOver)
-        newsBox.addEventListener('mouseout', handleMouseOut)
+        const newsBox = document.getElementById(`news-box${props.id}`)
+        newsBox.addEventListener('mouseenter', handleMouseEnter)
+        newsBox.addEventListener('mouseleave', handleMouseLeave)
     }
 
     const subscribeButton = createComponent(Button, {
         id: generateRandomId(10),
-        icon: isSubscribed(props.companyName) ? Icon.X : Icon.PLUS,
-        text: isSubscribed(props.companyName) ? '구독해제' : '구독하기',
+        icon: isSubscribed(props.news.id) ? Icon.X : Icon.PLUS,
+        text: isSubscribed(props.news.id) ? '구독해제' : '구독하기',
         variant: ButtonVariantProps.WHITE,
         onClick: () => {
-            handleSubscription(props.companyName)
+            handleSubscription(props.news.id)
         },
     })
 
-    const companyLogo = createComponent(IconView, {
+    const companyIcon = createComponent(IconView, {
         id: generateRandomId(10),
-        icon: props.icon,
+        icon: props.news.icon,
     })
 
     return {
         element: `
-        <div class="news-box-container" id="news-box-${props.id}">
-            ${isHover ? subscribeButton.element : companyLogo.element}
+        <div class="news-box-container" id="news-box${props.id}">
+            ${isHover ? subscribeButton.element : companyIcon.element}
         </div>
         `,
         bindEvents,
