3242b45f35b4dc5943ad9e39389c0ed3bf865ba8
DaeWon9 <qkreodnjs97@naver.com>
2024-07-08 15:50:07 +0900

    feat: cors

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const companyRoutes = require('./routes/companyRoutes')
 const newsRoutes = require('./routes/newsRoutes')
 
 const app = express()
+app.use(cors())
 const PORT = process.env.PORT || 3000
 
 app.use('/api', companyRoutes)
