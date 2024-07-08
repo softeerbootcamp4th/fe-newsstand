ac8112f9d16510a8765d6e67bb4e5ba6fb755e52
bluejoyq <bluejoy32@gmail.com>
2024-07-08 15:53:27 +0900

    feat: AppHeader 구현

--- src/components/AppHeader/index.ts
+++ src/components/AppHeader/index.ts
@@ -1,6 +1,26 @@
 import { NewspaperIcon } from "@/assets/NewspaperIcon";
-import { ce } from "@/libs/elements";
-
+import { Div, H1, Span, ce } from "@/libs/elements";
+import typoStyles from "@/styles/typo.module.css";
+import styles from "./index.module.css";
+import { formatHeaderDate } from "@/utils/formatDate";
 export const AppHeader = () => {
-  return ce(NewspaperIcon, {});
+  return ce(Div, {
+    className: styles.container,
+    children: [
+      ce(Span, {
+        className: styles["sub-container"],
+        children: [
+          ce(NewspaperIcon, {}),
+          ce(H1, {
+            className: typoStyles["display-bold24"],
+            children: ["뉴스스탠드"],
+          }),
+        ],
+      }),
+      ce(Span, {
+        className: `${styles.date} ${typoStyles["display-medium16"]}`,
+        children: [formatHeaderDate(new Date())],
+      }),
+    ],
+  });
 };
