d868f9906cd27a0d84b8e861f2b101f687a59b5d
DaeWon9 <qkreodnjs97@naver.com>
2024-07-08 14:09:47 +0900

    feat: 구독한 언론사일때, 데이터처리 추가

--- newsstand/src/components/category/MediaCategories.js
+++ newsstand/src/components/category/MediaCategories.js
@@ -2,12 +2,25 @@ import createComponent from '../../core/component/component.js'
 import { generateRandomId } from '../../utils/idGenerator.js'
 import { mediaCategoryData } from '../../datas/mockData.js'
 import CategoryBox from '../../components/category/CategoryBox.js'
+import { getSubscribedCompaniesId } from '../../utils/subscribeUtils.js'
+import { getCompanyName } from '../../datas/companyData.js'
 
 const MediaCategories = (props) => {
-    const categoryComponents = mediaCategoryData.map((category) =>
+    let categoryDatas = mediaCategoryData
+    if (props.selectedSource === '내가 구독한 언론사') {
+        let categoryIds = getSubscribedCompaniesId()
+
+        categoryDatas = categoryIds.map((id) => {
+            let companyName = getCompanyName(id)
+            return companyName
+        })
+    }
+
+    const categoryComponents = categoryDatas.map((category) =>
         createComponent(CategoryBox, {
             id: generateRandomId(10),
             text: category,
+            selectedSource: props.selectedSource,
             state: props.selectedCategory,
             setState: props.setSelectedCategory,
             currentNewsId: props.currentNewsId,
