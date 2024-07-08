363ae549538692666fd58f30d52c4d2b2ea06b33
bluejoyq <bluejoy32@gmail.com>
2024-07-08 16:57:37 +0900

    feat: rolling news 데이터 연결 및 스타일 완성

--- src/components/CurrentNews/RollingNews.ts
+++ src/components/CurrentNews/RollingNews.ts
@@ -1,21 +1,54 @@
-import { useState } from "@/libs";
-import { H1, Span, ce } from "@/libs/elements";
+import { useEffect, useState } from "@/libs";
+import { Div, H2, Span, ce } from "@/libs/elements";
 import styles from "./RollingNews.module.css";
+import { getRecentNews } from "@/remotes/getRecentNews";
+import { RecentNews } from "@/models/News";
+import typoStyles from "@/styles/typo.module.css";
+import { getMediaById } from "@/remotes/getMediaById";
+import { Media } from "@/models/Media";
 interface RollingNewsProps {
   delayed: boolean;
 }
 export const RollingNews = ({ delayed }: RollingNewsProps) => {
   const [idx, setIdx] = useState(0);
-
+  const [recentNews, setRecentNews] = useState<RecentNews | null>(null);
+  const [media, setMedia] = useState<Media | null>(null);
+  const loadData = async () => {
+    const recentNewsData = await getRecentNews({
+      from: idx * 2 + (delayed ? 1 : 0),
+    });
+    setRecentNews(recentNewsData);
+    const media = await getMediaById(recentNewsData.mediaId);
+    setMedia(media);
+  };
+  useEffect(() => {
+    loadData();
+  }, [idx]);
   return ce(Span, {
     className: `${styles["rolling-container"]}`,
     children: [
-      ce(H1, {
-        className: delayed ? styles["rolling-delay"] : styles["rolling"],
-        children: [`RollingNews ${idx}`],
-        onAnimationIteration: () => {
-          setIdx(idx + 1);
-        },
+      ce(Div, {
+        className: styles.box,
+        children: [
+          ce(H2, {
+            className: `${
+              delayed ? styles["rolling-delay"] : styles["rolling"]
+            } ${styles.text}`,
+            children: [
+              ce(Span, {
+                className: `${typoStyles["display-bold14"]} ${styles["media-name"]}`,
+                children: [media?.name],
+              }),
+              ce(Span, {
+                className: `${typoStyles["available-medium14"]} ${styles["news-title"]}`,
+                children: [recentNews?.news.title],
+              }),
+            ],
+            onAnimationIteration: () => {
+              setIdx(idx + 1);
+            },
+          }),
+        ],
       }),
     ],
   });
