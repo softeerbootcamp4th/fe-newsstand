e3a193c82a0e64faa7c793dd1f9840e5392622ba
darkdulgi <daniel99285@gmail.com>
2024-07-08 17:13:57 +0900

    feat : 뉴스데이터 뿌려주기

--- src/news.js
+++ src/news.js
@@ -2,8 +2,14 @@ import { updateDOMstyle } from "./util.js";
 
 const $subscribeToggleDOM = document.querySelectorAll(".subscribe-toggle span");
 const $newsgroupDOM = document.querySelector(".newsgroup");
+const $headlineTitleDOM = document.querySelector(".paper .headline span");
+const $newsListDOM = document.querySelector(".paper .list");
+const $comImgDOM = document.querySelector(".paper .subscribe-bar img");
+const $headlineImgDOM = document.querySelector(".paper .headline img");
 const categories = ["종합/경제", "방송/통신", "IT", "영자지", "스포츠/연예", "매거진/전문지", "지역"];
 const ctgScrollNum = 6;
+let newsList;
+let newsCom;
 
 let state = {
   subscribeToggle: "whole",
@@ -16,6 +22,7 @@ categories.forEach((ctg) => {
   const newnewDOM = document.createElement("span");
   newnewDOM.innerText = ctg;
   newDOM.appendChild(newnewDOM);
+  newDOM.setAttribute("class", "unselected");
   $newsgroupDOM.appendChild(newDOM);
 });
 
@@ -35,13 +42,7 @@ const ctgFill = () => {
     duration: 5000,
   };
 
-  updateDOMstyle(DOM, {
-    background: `linear-gradient(to right, #4362D0 50%, #7890E7 50%)`,
-    backgroundSize: "200%",
-    color: "white",
-    fontWeight: "bold",
-  });
-
+  DOM.setAttribute("class", "selected");
   DOM.animate(keyframes, options).onfinish = () => {
     ctgLose();
     state.comId++;
@@ -64,28 +65,43 @@ const ctgFill = () => {
 
 const ctgLose = () => {
   const DOM = $ctgDOM[state.ctg];
+  DOM.setAttribute("class", "unselected");
   const animations = DOM.getAnimations();
   animations.forEach(ani => ani.cancel());
 
-  updateDOMstyle(DOM, {
-    background: "inherit",
-    color: "inherit",
-    fontWeight: "normal"
-  });
-
-  if (DOM.childElementCount >= 2) {
+  while (DOM.childElementCount > 1) {
     DOM.removeChild(DOM.lastElementChild);
   }
 };
 
 const bottomFill = () => {
-  const $comImgDOM = document.querySelector(".paper .subscribe-bar img");
-  const $headlineImgDOM = document.querySelector(".paper .headline img");
   $comImgDOM.setAttribute("src", `../img/${state.comId}.png`);
   $headlineImgDOM.setAttribute("src", `../img/sample${state.comId}.jpg`);
+  const currentComName = newsCom.find((com) => com.id === state.comId).name;
+  const frontNewsList = newsList[categories[state.ctg]].filter((news) => news.com === currentComName);
+  let isFirstNews = true;
+
+  $headlineTitleDOM.innerText = frontNewsList[0] ? frontNewsList[0].title : "카테고리와 언론사에 해당되는 기사가 없습니다.";
+  while ($newsListDOM.firstChild) {
+    $newsListDOM.removeChild($newsListDOM.firstChild);
+  }
+  frontNewsList.forEach((news) => {
+    if (isFirstNews) isFirstNews = false;
+    else {
+      const newDOM = document.createElement("span");
+      newDOM.innerText = news.title;
+      $newsListDOM.appendChild(newDOM);
+    }
+  });
+  const newDOM = document.createElement("caption");
+  newDOM.innerText = `${currentComName} 언론사에서 직접 편집한 뉴스입니다.`;
+  $newsListDOM.appendChild(newDOM);
 }
 
 export default function CategoriesAndNewsSection(_news, _newsCom) {
+  newsList = _news;
+  newsCom = _newsCom;
+
   document.querySelector(".subscribe-toggle").addEventListener("click", (e) => {
     state.subscribeToggle = e.target.className;
     updateDOMstyle($subscribeToggleDOM[0], {
@@ -100,27 +116,32 @@ export default function CategoriesAndNewsSection(_news, _newsCom) {
 
   document.querySelector(".paper").addEventListener("click", (e) => {
     const targetClassName = e.target.className;
-    if (targetClassName !== "leftscroll" && targetClassName !== "rightscroll") return;
 
-    ctgLose();
     if (targetClassName === "leftscroll") {
+      ctgLose();
       state.comId--;
       if (state.comId === 0) {
         state.comId = ctgScrollNum;
         state.ctg--;
         if (state.ctg < 0) state.ctg = categories.length - 1;
       }
+      ctgFill();
+      bottomFill();
     }
-    else {
+    else if (targetClassName === "rightscroll") {
+      ctgLose();
       state.comId++;
       if (state.comId > ctgScrollNum) {
         state.comId = 1;
         state.ctg++;
         if (state.ctg === categories.length) state.ctg = 0;
       }
+      ctgFill();
+      bottomFill();
     }
-    ctgFill();
-    bottomFill();
+    else if(targetClassName === "subscribe-button"){
+
+    }
   });
 
   ctgFill();
