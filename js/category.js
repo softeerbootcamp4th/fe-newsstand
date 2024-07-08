fad87885e3d1f90277f3c47eeb852b50301c07bb
moana16 <moana16@naver.com>
2024-07-08 14:57:12 +0900

    style : 내 구독 헤더 화살표 추가

--- js/category.js
+++ js/category.js
@@ -137,15 +137,16 @@ function updateCategoryDisplay() {
     const categoryItems = document.querySelectorAll('.category-item');
     categoryItems.forEach((item, index) => {
         // 기존의 인덱스 요소 제거
-        const existingIndexElement = item.querySelector('.index-text');
+        const existingIndexElement = prevCategoryType === 'all' ? item.querySelector('.index-text') : item.querySelector('.arrow-icon');
         if (existingIndexElement) {
             existingIndexElement.remove();
         }
 
-        // 새로운 인덱스 요소 생성 및 추가
-        const textElement = document.createElement('span');
-        textElement.classList.add('index-text');
         if(prevCategoryType === 'all') {
+                    // 새로운 인덱스 요소 생성 및 추가
+            const textElement = document.createElement('span');
+            textElement.classList.add('index-text');
+
             if (index === curCategoryIdx) {
                 textElement.textContent = `${curNewsIdx + 1} / ${newsData[curCategoryIdx].news.length}`;
                 item.classList.add('selected-category'); 
@@ -153,11 +154,26 @@ function updateCategoryDisplay() {
                 item.classList.remove('selected-category'); 
             }
 
+            item.appendChild(textElement);
+
+
+        }
+        else {
+            const imgElement = document.createElement('img');
+            imgElement.classList.add('arrow-icon');
+            if (index === curCategoryIdx) {
+                imgElement.src = './src/icons/chevron-right.svg';
+                imgElement.style.display = 'block';
+                item.classList.remove('selected-category'); 
+
+            } else {
+                item.classList.remove('selected-category'); 
+            }
+            item.appendChild(imgElement);
         }
 
         
 
-        item.appendChild(textElement);
     });
 }
 
