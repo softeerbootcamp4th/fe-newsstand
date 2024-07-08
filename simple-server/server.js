6000c9a28717734d418d2b948ab1dd644706adea
DaeWon9 <qkreodnjs97@naver.com>
2024-07-08 18:14:40 +0900

    feat: 서버에 JSON 파싱 미들웨어 추가

--- simple-server/server.js
+++ simple-server/server.js
@@ -6,6 +6,8 @@ const newsRoutes = require('./routes/newsRoutes')
 
 const app = express()
 app.use(cors())
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 
 app.use('/api', companyRoutes)
