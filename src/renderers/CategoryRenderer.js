5eb9bb9a982129a1de28ad626e71b5244ea043a2
jw0097 <jwoo27@hanmail.net>
2024-07-09 18:17:02 +0900

    refactor: 렌더 함수를 위한 클래스 생성

---
+++ src/renderers/CategoryRenderer.js
@@ -0,0 +1,39 @@
+import { Renderer } from "./Renderer.js";
+
+/**
+ * @class CategoryRenderer
+ * @classdesc 카테고리를 렌더링하는 클래스
+ */
+class CategoryRenderer extends Renderer {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+        this.categoryContainer = document.querySelector(".news-list__navbar");
+    }
+
+    /**
+     * 이 클래스에서 실행 될 이벤트인지 확인한다.
+     * @param {string} event.eventName - 이벤트의 이름 
+     * @returns 
+     */
+    checkEventName(event) {
+        return event.eventName === 'clickCategory';
+    }
+
+    /**
+     * 렌더 함수
+     */
+    render({ eventName, value, ...rest }) {
+        const categoryListElement = this.#generateCategoryListElement(value);
+        this.categoryContainer.innerHTML = '';
+        this.categoryContainer.innerHTML += categoryListElement;
+    }
+
+    #generateCategoryListElement(value) {
+        return `<div>dd</div>`
+    }
+}
+
+export { CategoryRenderer }
