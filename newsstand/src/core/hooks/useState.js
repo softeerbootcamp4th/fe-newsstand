f8e68da68c8d458c7eea688c552b1b8ebcb953dc
DaeWon9 <qkreodnjs97@naver.com>
2024-07-09 18:37:28 +0900

    feat: debounce 기능 추가

--- newsstand/src/core/hooks/useState.js
+++ newsstand/src/core/hooks/useState.js
@@ -4,6 +4,18 @@ import { deepEqual } from '../../utils/deepEqual.js'
 
 export const componentsState = new Map()
 
+const debounce = (func, wait) => {
+    let timeout
+    return (...args) => {
+        const later = () => {
+            clearTimeout(timeout)
+            func(...args)
+        }
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+    }
+}
+
 const useState = ({ stateId, initialValue }) => {
     const { id, component, componentProps } = currentComponent
     const uniqueId = `${id}_${stateId}`
@@ -18,16 +30,17 @@ const useState = ({ stateId, initialValue }) => {
         const currentState = state
         const updatedState = typeof newValue === 'function' ? newValue(currentState) : newValue
 
-        const shouldUpdate = deepEqual(currentState, updatedState)
+        const shouldUpdate = !deepEqual(currentState, updatedState)
 
-        if (!shouldUpdate) {
-            componentsState.set(uniqueId, newValue)
-
+        if (shouldUpdate) {
+            componentsState.set(uniqueId, updatedState)
             render(component, componentProps)
         }
     }
 
-    return [{ stateId: stateId, value: state }, setState]
+    const debouncedSetState = debounce(setState, 30)
+
+    return [{ stateId: stateId, value: state }, debouncedSetState]
 }
 
 export default useState
