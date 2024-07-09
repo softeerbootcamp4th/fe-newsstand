187c2c9627cf6ecdd52737bbdf7f4153b9cb09f0
subsub-e <ab13696802@gmail.come>
2024-07-09 16:53:07 +0900

    [develop] localstorage 로직 구현

--- src/components/newsListLi.js
+++ src/components/newsListLi.js
@@ -1,7 +1,10 @@
 import { allNewsData } from './news.js';
+import { getSubscribedData } from './fieldTab.js';
 
 export const newsListLi = (isFull) => {
     const NewsListUl = document.getElementById('NewsList').querySelector('ul');
+    const subscribed = getSubscribedData();
+
     if (isFull) {
         return allNewsData.map((item) => {
             const li = document.createElement('li');
@@ -13,18 +16,15 @@ export const newsListLi = (isFull) => {
         });
     }
     else {
-        return allNewsData.reduce((acc, category) => {
-            category.company.forEach((company) => {
-                if (company.isSubscribe) {
-                    const li = document.createElement('li');
-                    li.textContent = company.companyName;
-                    li.classList.add('notselectNews');
+        if (subscribed.length != 0) {
+            return subscribed.map((item) => {
+                const li = document.createElement('li');
+                li.textContent = item;
+                li.classList.add('notselectNews');
 
-                    NewsListUl.appendChild(li);
-                    acc.push(li);
-                }
+                NewsListUl.appendChild(li);
+                return li;
             });
-            return acc;
-        }, []);
+        }
     }
 };
