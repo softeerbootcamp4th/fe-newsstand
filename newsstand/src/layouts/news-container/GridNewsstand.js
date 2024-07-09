6007130db730c47776e1dc3fc1ffcfff10ccd984
DaeWon9 <qkreodnjs97@naver.com>
2024-07-09 16:59:41 +0900

    refactor: useEffect && fectch api 로 데이터를 처리하도록 리팩토링

--- newsstand/src/layouts/news-container/GridNewsstand.js
+++ newsstand/src/layouts/news-container/GridNewsstand.js
@@ -2,46 +2,70 @@ import NewsBox from '../../components/news/NewsBox.js'
 import createComponent from '../../core/component/component.js'
 import IconView, { Icon } from '../../components/base/IconView.js'
 import useState from '../../core/hooks/useState.js'
-import { wholeCompanyData, getCompanyDataByPage, getMaxPage } from '../../datas/companyData.js'
-import { generateRandomId } from '../../utils/idGenerator.js'
+import { wholeCompanyData } from '../../datas/companyData.js'
+import { fetchMaxPageNumber } from '../../api/companyApis.js'
+import { fetchCompanyDataByPage } from '../../api/companyApis.js'
 import { getSubscribedCompanies } from '../../utils/subscribeUtils.js'
+import useEffect from '../../core/hooks/useEffect.js'
 
 const GridNewsstand = (props) => {
     const [page, setPage] = useState({ stateId: 1, initialValue: 1 })
-    const maxPage = getMaxPage()
+    const [maxPage, setMaxPage] = useState({ stateId: 2, initialValue: 0 })
+    const [companyData, setCompanyData] = useState({ stateId: 3, initialValue: [] })
 
-    let companyData = []
-
-    if (props.selectedSource === '전체 언론사') {
-        companyData = getCompanyDataByPage(page)
-    } else {
-        companyData = getSubscribedCompanies(wholeCompanyData)
+    const initCompanyData = () => {
+        if (props.viewType.value === 'list') return
+        if (props.selectedSource.value === '전체 언론사') initWholeCompaniesData()
+        else initSubscribedCompaniesData()
     }
 
-    const newsBoxes = companyData.map((news) => {
+    const initWholeCompaniesData = () => {
+        fetchCompanyDataByPage(page.value).then((res) => {
+            setCompanyData(res)
+
+            fetchMaxPageNumber().then((res) => {
+                setMaxPage(res.maxPage)
+            })
+        })
+    }
+
+    const initSubscribedCompaniesData = () => {
+        getSubscribedCompanies(wholeCompanyData)
+            .then((res) => {
+                setCompanyData(res)
+                return res
+            })
+            .then((res) => {
+                setMaxPage(Math.ceil(res.length / 24)) // todo magicNumber -> constant
+            })
+    }
+
+    useEffect(() => initCompanyData(), [props.viewType, props.selectedSource, page], 1)
+
+    const newsBoxes = companyData.value.map((news, index) => {
         return createComponent(NewsBox, {
-            id: generateRandomId(10),
+            id: `${props.viewType}-${index}`,
             news: news,
             style: 'width: 100%; height:100%;',
         })
     })
 
     const leftButtonIcon = createComponent(IconView, {
-        id: generateRandomId(10),
+        id: 'grid-left-btn-1',
         icon: Icon.LEFT_BUTTON,
     })
 
     const rightButtonIcon = createComponent(IconView, {
-        id: generateRandomId(10),
+        id: 'grid-right-btn-1',
         icon: Icon.RIGHT_BUTTON,
     })
 
     const handleRightButtonClick = () => {
-        if (page < maxPage) setPage(page + 1)
+        if (page.value < maxPage.value) setPage(page.value + 1)
     }
 
     const handleLeftButtonClick = () => {
-        if (page > 1) setPage(page - 1)
+        if (page.value > 1) setPage(page.value - 1)
     }
 
     const bindEvents = () => {
@@ -56,13 +80,13 @@ const GridNewsstand = (props) => {
         element: `
         <div class="carousel-container">
             <button class="carousel-btn left-btn">
-                ${page > 1 ? leftButtonIcon.element : ''}
+                ${page.value > 1 ? leftButtonIcon.element : ''}
             </button>
             <div class="grid-news-container">
                 ${newsBoxes.map((box) => box.element).join('')}
             </div>
             <button class="carousel-btn right-btn">
-                ${page < maxPage ? rightButtonIcon.element : ''}
+                ${page.value < maxPage.value ? rightButtonIcon.element : ''}
             </button>
         </div>
         `,
