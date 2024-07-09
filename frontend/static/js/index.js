314112585b5bf35d0ac3ac3731c239d7097d325e
pccommen <pccommen@gmail.com>
2024-07-09 18:23:16 +0900

    refactor: dom의 생성주기에 맞춰 reducer 및 컴포넌트 초기화

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,41 @@
-import "./NewsstandTitle/index.js";
-import "./mainContentControl/index.js";
-import "./recentNews/index.js";
+import { NEWS } from "../data/data.js";
+import * as mainContent from "./mainContent/index.js";
+import * as mainContentSelector from "./mainContentSelector/index.js";
+import * as title from "./newsstandTitle/index.js";
+import { reducer, reducerInit } from "./reducer.js";
+import * as ticker from "./ticker/index.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const template = reducerInit(
+        [
+            title.actions,
+            ticker.actions,
+            mainContentSelector.actions,
+            mainContent.actions
+        ],
+        title.init(),
+        ticker.init(),
+        mainContentSelector.init(),
+        mainContent.init()
+    )
+
+    document.body.addEventListener('click', event => reducer.click(event))
+    document.getElementById("newsstand").insertAdjacentHTML("afterbegin", template);
+    ticker.domDidLoad(NEWS)
+    mainContent.domDidLoad(NEWS)
+});
+
+const stylesheets = [
+    "static/js/newsstandTitle/newstandTitle.css",
+    "static/js/ticker/ticker.css",
+    "static/js/mainContentSelector/mainContentSelector.css"
+];
+
+stylesheets.forEach(loadCSS);
+
+function loadCSS(url) {
+    document.head.insertAdjacentHTML(
+        "beforeend",
+        `<link rel='stylesheet' type='text/css' href='${url}'/>`,
+    );
+}
