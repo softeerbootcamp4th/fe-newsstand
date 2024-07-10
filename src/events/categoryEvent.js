2ff7a5036cc059b7697076898432c267e903a32f
jw0097 <jwoo27@hanmail.net>
2024-07-10 16:43:38 +0900

    Comment: deepfreeze와 이벤트 리스너 함수들에 대해 주석 추가

--- src/events/categoryEvent.js
+++ src/events/categoryEvent.js
@@ -1,5 +1,8 @@
 import { deepFreeze } from "../utils/deepfreeze.js";
 
+/**
+ * 카테고리를 클릭 했을 때의 이벤트 정보 객체
+ */
 export const categoryClickEventInfo = deepFreeze({
     type: "click",
     listener: (event, states, element) => {
@@ -14,4 +17,7 @@ export const categoryOnloadEventInfo = deepFreeze({
     }
 })
 
-export const categoryEventTypeList = deepFreeze(["load", "click"]);
+/**
+ * Renderer에서 렌더를 허용할 이벤트 타입의 목록
+ */
+export const categoryEventTypeList = deepFreeze(["click"]);
