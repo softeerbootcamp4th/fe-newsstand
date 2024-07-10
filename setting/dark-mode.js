d9c993683cb322b465122ffa8d61ed3ca9f15cc8
jhj2713 <juhyojeong58@gmail.com>
2024-07-10 14:16:02 +0900

    feat: 다크모드 토글 로직 구현

--- setting/dark-mode.js
+++ setting/dark-mode.js
@@ -1,4 +1,4 @@
-import { getItem } from "../utils/local-storage.js";
+import { getItem, setItem } from "../utils/local-storage.js";
 
 /**
  * @description 다크모드 설정을 렌더하는 함수
@@ -13,25 +13,37 @@ export function renderDarkmode() {
  * @description 다크모드 버튼을 렌더하는 함수
  */
 function renderDarkmodeButton() {
-    const bodyDOM = document.querySelector("body");
+    const darkmodeButtonDOM = document.querySelector(".dark-mode__button");
     const isDarkmode = getItem("dark-mode") ?? false;
 
-    const darkmodeButtonDOMString = `
-    <button class="dark-mode__button">
-        ${isDarkmode ? `
-            <img class="dark-mode__icon" alt="라이트모드 전환 아이콘" src="./static/icons/moon.png" />
+    const darkmodeButtonDOMString = isDarkmode ? `
+            <img class="dark-mode__icon dark-mode__icon--dark" alt="라이트모드 전환 아이콘" src="./static/icons/moon.png" />
             ` : `
             <img class="dark-mode__icon" alt="다크모드 전환 아이콘" src="./static/icons/sun.png" />
-            `}
-    </button>
-    `
+            `;
 
-    bodyDOM.insertAdjacentHTML("beforeend", darkmodeButtonDOMString);
+    darkmodeButtonDOM.innerHTML = darkmodeButtonDOMString;
+
+    const bodyDOM = document.querySelector("body");
+    if (isDarkmode) {
+        bodyDOM.classList.add("dark-mode");
+    } else {
+        bodyDOM.classList.remove("dark-mode");
+    }
 }
 
 /**
  * @description 다크모드 관련 로직을 처리하는 함수
  */
 function setDarkmode() {
+    const darkmodeButtonDOM = document.querySelector(".dark-mode__button");
 
+    darkmodeButtonDOM.addEventListener("click", clickDarkMode);
+}
+
+function clickDarkMode() {
+    const isDarkmode = getItem("dark-mode") ?? false;
+    setItem("dark-mode", !isDarkmode);
+
+    renderDarkmodeButton();
 }
