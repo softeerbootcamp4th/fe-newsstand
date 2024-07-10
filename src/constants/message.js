211e7f8f145e969e140329e1f6ec58b3c1c33bed
jw0097 <jwoo27@hanmail.net>
2024-07-10 16:30:38 +0900

    feat: 객체 deep freeze 함수 적용

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,3 +1,5 @@
-export const ERROR_MESSAGE = Object.freeze({
+import { deepFreeze } from "../utils/deepfreeze.js";
+
+export const ERROR_MESSAGE = deepFreeze({
     NOT_IMPLEMENTED: "메서드 구현이 필요합니다."
 })
--- src/events/categoryEvent.js
+++ src/events/categoryEvent.js
@@ -1,13 +1,17 @@
-export const categoryClickEventInfo = {
+import { deepFreeze } from "../utils/deepfreeze.js";
+
+export const categoryClickEventInfo = deepFreeze({
     type: "click",
     listener: (event, states, element) => {
         states.setCategory(Array.from(element.children).indexOf(event.target))
     }
-}
+})
 
-export const categoryOnloadEventInfo = {
+export const categoryOnloadEventInfo = deepFreeze({
     type: "load",
     listener: (event, states, element) => {
         console.log(1)
     }
-}
+})
+
+export const categoryEventTypeList = deepFreeze(["load", "click"]);
