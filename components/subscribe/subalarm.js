b9b27b2dbd082f05092c75aed9d2844c8baa19e3
fairwheel3027148 <89960061+fairwheel3027148@users.noreply.github.com>
2024-07-10 10:26:19 +0900

    fix. subscribe/cancel modal event

--- components/subscribe/subalarm.js
+++ components/subscribe/subalarm.js
@@ -1,3 +1,6 @@
+import { mytabs } from "../newstab/newstab.js";
+import { pressDataArray } from "./subscribe.js";
+
 export const showsubmodal = () => {
     const modal = document.getElementById('submodal');
     modal.innerText = "내가 구독한 언론사에 추가되었습니다.";
@@ -7,11 +10,33 @@ export const showsubmodal = () => {
     }, 5000);
 }
 
-export const showcancelmodal = (bt_text) =>{
-    const modal = document.getElementById('cancelmodal');
-    modal.innerText = bt_text+"을(를) 구독해지하겠습니까?";
-    modal.className = 'active';
-    setTimeout(() => {
-        modal.classList.remove('active');
-    }, 5000);
-}
+// 모달 창 닫기 함수
+const closeModal = () => {
+    const modalContainer = document.querySelector('.cancel-modal-container');
+    modalContainer.classList.remove('show');
+};
+
+//구독 취소 창
+export const showCancelmodal = (btn_text) => {
+    const modalContainer = document.querySelector('.cancel-modal-container');
+    modalContainer.classList.add('show');
+    const modalText = document.getElementById('cancel-modal-text');
+    modalText.textContent = `${btn_text}`;
+};
+
+const cancelyes = document.querySelector('.cancel-yes');
+const cancelno = document.querySelector('.cancel-no');
+
+cancelyes.addEventListener('click', () => {
+    // 먼저 창닫기
+    closeModal();
+    let modaltxt = document.getElementById('cancel-modal-text');
+    const btn = document.querySelector(`.news-press-subscribe`);
+    btn.innerHTML = '<img src="../../icons/Subscribe.svg" alt="Subscribe">';
+    let sss = pressDataArray.filter(item => item !== modaltxt.textContent);
+    let updatedsubs = JSON.stringify(sss);
+    localStorage.setItem('mysubscribe' , updatedsubs);
+    mytabs();
+});
+
+cancelno.addEventListener('click', () => {closeModal()});
