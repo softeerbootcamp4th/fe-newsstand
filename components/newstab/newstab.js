82f52663fbe4dc6dc79533594f8cb75429310a67
fairwheel3027148 <89960061+fairwheel3027148@users.noreply.github.com>
2024-07-10 14:45:20 +0900

    fix. innerHTML bug

--- components/newstab/newstab.js
+++ components/newstab/newstab.js
@@ -1,13 +1,18 @@
 import { shownewstab } from "../displaynews/displaysubscribe.js";
-import { updateNewsDisplay } from "../displaynews/displayNews.js";
-import { resetProgress, transformToProgress } from "../progressbar/progressbutton.js";
 
 export const mytabs = () => {
     const datas1 = localStorage.getItem("mysubscribe");
     if (!datas1) {
-        const button = document.querySelector(`.news-list-header`);
-        button.innerHTML = '';
         console.log('No data found in localStorage');
+        const button = document.querySelector(`.news-list-header`);
+        
+        button.innerHTML = '';
+        const lists = document.querySelector(".news-main-container");
+        lists.innerHTML = '';
+        const lists1 = document.querySelector(".news-list-header");
+        lists1.innerHTML = '';
+        const lists2 = document.querySelector(".news-press-container");
+        lists2.innerHTML = ''; 
         return;
     }
 
@@ -23,29 +28,11 @@ export const newstype = ["economy", "broadcast", "internet", "englishnews", "spo
 export const originaltabs = () => {
     const defaultn = document.querySelector(".news-list-header");
     defaultn.innerHTML = 
-    "<article class=\"text-button\" id=\"economy\ data-index=\"0\">종합/경제 <span></span></article>"+
+    "<article class=\"text-button\" id=\"economy\" data-index=\"0\">종합/경제 <span></span></article>"+
     "<article class=\"text-button\" id=\"broadcast\" data-index=\"1\">방송/통신 <span></span></article>"+
     "<article class=\"text-button\" id=\"internet\" data-index=\"2\">IT <span></span></article>"+
     "<article class=\"text-button\" id=\"englishnews\" data-index=\"3\">영자지 <span></span></article>"+
     "<article class=\"text-button\" id=\"sportsentertain\" data-index=\"4\">스포츠/연예 <span></span></article>"+
     "<article class=\"text-button\" id=\"magazine\" data-index=\"5\">매거진/전문지 <span></span></article>"+
     "<article class=\"text-button\" id=\"region\" data-index=\"6\">지역 <span></span></article>";
-}
-
-export const tabgenerator = () => {
-    let tempidx = 0;
-    const buttons = document.querySelectorAll('.text-button');
-    buttons.forEach(bt => {
-        //console.log(bt.id);
-        bt.addEventListener('click', (event) => { 
-            const a = event.target;
-            tempidx = 0;
-            updateNewsDisplay(a.id, tempidx);
-            resetProgress();
-            transformToProgress(bt);     
-        });
-    });
-    updateNewsDisplay("economy", tempidx);
-    const ts = document.querySelector(".text-button");
-    transformToProgress(ts);
 }
