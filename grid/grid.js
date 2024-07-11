8acf4e984f4f315d87b94dca03e8702e1b0352b8
admin <admin@adminui-noteubug.local>
2024-07-11 16:49:11 +0900

    feat: grit화면에서도 구독한 언론사 표시

--- grid/grid.js
+++ grid/grid.js
@@ -1,22 +1,39 @@
-import { mediaData } from "../resources/data.js";
+import { getMyDataAsArray, mediaData } from "../resources/data.js";
 import { generateNode } from "../utils/utils.js";
 
 export function generateGridContent(container, categoryIndex) {
+  let currentGridIndex = 0;
+
   container.classList.remove("list");
   container.classList.add("grid");
   const divNewsGridWrapper = generateNode("ul", "newsGrid_wrapper");
 
-  const mediaList = Object.entries(mediaData).map((element) => {
+  let mediaPageList = [];
+  if (categoryIndex === 0) mediaPageList = getGridList(mediaData);
+  else if (categoryIndex === 1) mediaPageList = getGridList(getMyDataAsArray());
+
+  renderGridItems(divNewsGridWrapper, mediaPageList[currentGridIndex]);
+
+  container.appendChild(divNewsGridWrapper);
+}
+
+function getGridList(mediaObjectList) {
+  let girdList = [];
+
+  Object.entries(mediaObjectList).forEach((element, index) => {
     const object = { mediaId: element[0], media: element[1].media };
-    return object;
+    if (index % 24 === 0) girdList[Math.floor(index / 24)] = new Array();
+    girdList[Math.floor(index / 24)].push(object);
   });
 
-  mediaList.forEach((element) => {
+  return girdList;
+}
+
+function renderGridItems(container, itemList) {
+  itemList.forEach((element) => {
     const item = generateNode("li", "gridItem");
     item.innerHTML = element.media;
 
-    divNewsGridWrapper.appendChild(item);
+    container.appendChild(item);
   });
-
-  container.appendChild(divNewsGridWrapper);
 }
