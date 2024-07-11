2413aa0a1d4d1289bd7fe6e392168edec4ad3b53
pccommen <pccommen@gmail.com>
2024-07-11 16:29:29 +0900

    feature: Fetcher를 상속받은 NewsstandFetcher 구현

---
+++ frontend/static/js/apis/newstandFetcher.js
@@ -0,0 +1,15 @@
+import Fetcher from "./fetcher.js";
+
+class NewsstandFetcher extends Fetcher {
+    constructor() {
+        super("http://localhost:3001/data");
+    }
+
+    getAllCompanies = async () => await super.get("")
+    getMyCompanies = async () => (await super.get("")).filter(element => element.isSubscribe)
+    updateSubscribe = async (id, isSubscribe) => await super.patch(isSubscribe, id)
+}
+
+const newsstandFetcher = new NewsstandFetcher()
+
+export default newsstandFetcher
