590bab1ee4dd09ec05cfff76b7afbfc40ba8d541
pccommen <pccommen@gmail.com>
2024-07-11 16:28:39 +0900

    feat: Fetch class를 통해 REST API와의 인터페이스 구현

---
+++ frontend/static/js/apis/fetcher.js
@@ -0,0 +1,43 @@
+class Fetcher {
+    constructor(baseURL) {
+        this.baseURL = baseURL
+    }
+
+    async get(endpoint) {
+        const response = await fetch(`${this.baseURL}/${endpoint}`)
+
+        if (!response.ok) {
+            throw new Error("request failed.")
+        }
+
+        try {
+            const data = await response.json()
+            return data
+        }
+        catch {
+            throw new Error("parsing failed.")
+        }
+    }
+
+    async patch(data, endpoint) {
+        const jsonData = JSON.stringify(data);
+        const response = await fetch(`${this.baseURL}/${endpoint}`, {
+            method: 'PATCH',
+            body: jsonData
+        })
+        if (!response.ok) {
+            throw new Error("request failed.")
+        }
+
+        try {
+            const data = await response.json()
+            return data
+        }
+        catch {
+            throw new Error("parsing failed.")
+        }
+    }
+}
+
+
+export default Fetcher
