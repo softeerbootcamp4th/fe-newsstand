1dbc5432ca2d13458f4d84377800e9c56ddf4816
guridaek <xylitol79@gmail.com>
2024-07-12 11:54:55 +0900

    fix: mockNews 언론사 이름 익명 변경

--- src/data/mocks/news.js
+++ src/data/mocks/news.js
@@ -2,9 +2,8 @@ import news from "./news.json";
 
 export function getMockNews(companyId) {
   const newsString = JSON.stringify(news);
-  newsString.replace("연합뉴스", "OO뉴스");
 
-  const data = JSON.parse(newsString);
+  const data = JSON.parse(newsString.replaceAll("연합뉴스", "OO뉴스"));
 
   const idString = companyId.toString().padStart(3, "0");
 
