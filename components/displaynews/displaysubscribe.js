281885c0cbfcc9f1a45319a69f1046c2dd1d3951
fairwheel3027148 <89960061+fairwheel3027148@users.noreply.github.com>
2024-07-12 11:27:52 +0900

    fix. 파일 이름

--- components/displaynews/displaysubscribe.js
+++ components/displaynews/displaysubscribe.js
@@ -1,100 +1,124 @@
 import { showsubscribe } from "../subscribe/subscribe.js";
-
+import stateManager from "../statemanager/stateManager.js";
 //내가 구독한 뉴스기사에 따른  
-export const showmysub = (subitem) => {
+export const showmysub = (subscribedItem) => {
     clearInterval(subProgressTimer);
-    document.querySelector('.news-press-img img').src = subitem.pressImg;
-    document.querySelector('.news-press-edit').textContent = subitem.edittime;
-    document.querySelector('.news-image-container img').src = subitem.mainphoto;
-    document.querySelector('.news-title').textContent = subitem.maintitle;
-    document.querySelector('.text-button').textContent = subitem.pressName;
+    if (!subscribedItem) {
+        console.error('구독한 언론사가 없습니다.');
+        return;
+    }
+    document.querySelector('.news-press-img img').src = subscribedItem.pressImg || '';
+    document.querySelector('.news-press-edit').textContent = subscribedItem.edittime || '';
+    document.querySelector('.news-image-container img').src = subscribedItem.mainphoto || '';
+    document.querySelector('.news-title').textContent = subscribedItem.maintitle || '';
+    document.querySelector('.text-button').textContent = subscribedItem.pressName || '';
     const newsSubTitles = document.querySelectorAll('.news-sub-titles');
-    subitem.news.forEach((news, index) => {
-        newsSubTitles[index].textContent = news.title;
-        newsSubTitles[index].href = news.url;
+    subscribedItem.news.forEach((news, index) => {
+        newsSubTitles[index].textContent = news.title || '';
+        newsSubTitles[index].href = news.url || '';
     });
 }
 
 export let subProgressTimer;
-//currentIndex 전역변수와 구별 (구독만을 위한 변수)
 let subsindex = 0;
 
+var subscribedNews;
 //로컬 스토리지에 있는 아이템들에 대한 렌더링
-export const shownewstab = (fnews) => {
-    //clearInterval(subProgressTimer);
-    console.log(fnews);
+export const shownewstab = (news) => {
+    if (!news || !Array.isArray(news)) {
+        console.error('구독한 언론사가 없습니다.');
+        return;
+    }
     const tabinfos = document.querySelector('.news-list-header');
     tabinfos.innerHTML = '';
 
-    fnews.forEach(it => {
+    news.forEach(it => {
         tabinfos.innerHTML += `<article class="text-button">${it}</article>`;
     });
 
-    const tbuttons = document.querySelectorAll('.text-button');
+    subscribedNews = document.querySelectorAll('.text-button');
 
-    const updateTab = (index) => {
-        tbuttons.forEach((btn, idx) => {
-            if (idx === index) {
-                
-                // 애니메이션 초기화 후 다시 추가
-                btn.classList.remove('animate');
-                void btn.offsetWidth;  // Reflow를 발생시켜 애니메이션 재시작
-                btn.classList.add('subprogress-button', 'animate');
-
-                // 이미지 추가
-                if (!btn.querySelector('img')) {
-                    const img = document.createElement('img');
-                    img.src = '../../icons/chevron-right.svg';
-                    img.alt = 'Icon';
-                    btn.appendChild(img);
-                }
-                
-            } else {
-                btn.classList.remove('subprogress-button', 'animate');
-                const img = btn.querySelector('img');
-                if (img) {
-                    btn.removeChild(img);
-                }
-            }
-        });
-
-        const buttonText = tbuttons[index].textContent;
-        console.log(buttonText);
-        window.btntext = buttonText;
-        updateSubDisplay(buttonText);
-    };
-
-    tbuttons.forEach((bbtt, index) => {
-        bbtt.addEventListener('click', () => {
+    subscribedNews.forEach((subs, index) => {
+        subs.addEventListener('click', () => {
             subsindex = index;
             updateTab(subsindex);
         });
     });
     
     updateTab(subsindex);
-
     clearInterval(subProgressTimer);
+
     subProgressTimer = setInterval(() => {
-        subsindex = (subsindex + 1) % tbuttons.length;
+        subsindex = (subsindex + 1) % subscribedNews.length;
         updateTab(subsindex);
     }, 2000);
 };
 
+const updateTab = (index) => {
+    subscribedNews.forEach((btn, idx) => {
+        if (idx === index) {
+            
+            // 애니메이션 초기화 후 다시 추가
+            btn.classList.remove('animate');
+            void btn.offsetWidth;  // Reflow를 발생시켜 애니메이션 재시작
+            btn.classList.add('subprogress-button', 'animate');
+
+            // 이미지 추가
+            if (!btn.querySelector('img')) {
+                const img = document.createElement('img');
+                img.src = '../../icons/chevron-right.svg';
+                img.alt = 'Icon';
+                btn.appendChild(img);
+            }
+            
+        } else {
+            btn.classList.remove('subprogress-button', 'animate');
+            const img = btn.querySelector('img');
+            if (img) {
+                btn.removeChild(img);
+            }
+        }
+    });
+    if (subscribedNews){
+        const buttonText = subscribedNews[index]?.textContent;
+        stateManager.setClickedNews(buttonText);
+        updateSubDisplay(buttonText);
+    }
+};
+
 //관심 언론사 선택하였을 때, 페이지 넘김에 따른 화면 보여줌
-export const updateSubDisplay = (pname) => {
-    const filtered = window.newsData.filter(item => item.pressName === pname);
-    const news1 = filtered[0];
-    if (news1) {
-        document.querySelector('.news-press-img img').src = news1.pressImg;
-        document.querySelector('.news-press-edit').textContent = news1.edittime;
-        document.querySelector('.news-image-container img').src = news1.mainphoto;
-        document.querySelector('.news-title').textContent = news1.maintitle;
-        document.querySelector('.pressedby').textContent = news1.pressName + " 언론사에서 직접 편집한 뉴스입니다.";
+export const updateSubDisplay = (press) => {
+    const [selectedPress] = stateManager.getAllNews().filter(item => item.pressName === press);    
+    if (selectedPress) {
+        document.querySelector('.news-press-img img').src = selectedPress.pressImg;
+        document.querySelector('.news-press-edit').textContent = selectedPress.edittime;
+        document.querySelector('.news-image-container img').src = selectedPress.mainphoto;
+        document.querySelector('.news-title').textContent = selectedPress.maintitle;
+        document.querySelector('.pressedby').textContent = selectedPress.pressName + " 언론사에서 직접 편집한 뉴스입니다.";
         const newsSubTitles = document.querySelectorAll('.news-sub-titles');
-        news1.news.forEach((news, index) => {
+        selectedPress.news.forEach((news, index) => {
             newsSubTitles[index].textContent = news.title;
             newsSubTitles[index].href = news.url;
         });
-        showsubscribe(news1.pressName);
+        showsubscribe(selectedPress.pressName);
+    }
+    else{
+        document.querySelector('.news-press-img img').src = '';
+        document.querySelector('.news-press-edit').textContent = '';
+        document.querySelector('.news-image-container img').src = '';
+        document.querySelector('.news-title').textContent = '';
+        document.querySelector('.pressedby').textContent = ``;
+        const newsSubTitles = document.querySelectorAll('.news-sub-titles');
+        newsSubTitles.forEach((_, index) => {
+            if (newsSubTitles[index]) {
+                newsSubTitles[index].textContent =  '';
+                newsSubTitles[index].href = '#';
+            }
+        });
+        /*
+        alert("구독한 언론사가 없습니다.");
+        clickArt('all-article');
+        originaltabs();
+        initmain(); */
     }
 }
