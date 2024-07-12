fb2f5efb555daa9687cb8a677b4d9d344a1b1b71
darkdulgi <daniel99285@gmail.com>
2024-07-12 11:56:17 +0900

    feat : 그리드 뷰에서 마우스 호버시 구독/구독해제 버튼 나타나게 함

--- src/grid.js
+++ src/grid.js
@@ -1,4 +1,4 @@
-import { updateDOMstyle, getSubscribeList } from "./util.js";
+import { updateDOMstyle, getSubscribeList, removeSubscribe, pushSubscribe } from "./util.js";
 
 const $listButtonDOM = document.querySelector(".toggle .list-button");
 const $gridButtonDOM = document.querySelector(".toggle .grid-button");
@@ -18,33 +18,45 @@ const insertDOM = () => {
 
 const refillCompany = () => {
   $gridDOM.innerHTML = "";
-  if (state.subscribeToggle === "whole") {
-    for (let i = 1; i <= companyPerPage; i++) {
-      const newCellDOM = document.createElement("div");
-      newCellDOM.setAttribute("class", "cell");
-      if (i <= newsCompanyList.length) {
-        const newImgDOM = document.createElement("img");
-        newImgDOM.setAttribute("src", `../img/${i}.png`);
-        newCellDOM.appendChild(newImgDOM);
-      }
-      $gridDOM.appendChild(newCellDOM);
+  const isStateWhole = state.subscribeToggle === "whole";
+
+  for (let i = 0; i < companyPerPage; i++) {
+    const newCellDOM = document.createElement("div");
+    newCellDOM.setAttribute("class", "cell");
+    if (i < (isStateWhole ? newsCompanyList.length : getSubscribeList().length)) {
+      const newImgDOM = document.createElement("img");
+      const newButtonDOM = document.createElement("button");
+      const companyId = isStateWhole ? i + 1 : getSubscribeList()[i];
+      const isSubscribed = getSubscribeList().some((com) => com === companyId);
+
+      newImgDOM.setAttribute("src", `../img/${companyId}.png`);
+      newButtonDOM.innerText = isSubscribed ? "× 해지하기" : "+ 구독하기";
+      newCellDOM.addEventListener("mouseenter", () => {
+        updateDOMstyle(newImgDOM, { display: "none" });
+        updateDOMstyle(newButtonDOM, { display: "block" });
+      });
+      newCellDOM.addEventListener("mouseleave", () => {
+        updateDOMstyle(newImgDOM, { display: "block" });
+        updateDOMstyle(newButtonDOM, { display: "none" });
+      });
+
+      newButtonDOM.addEventListener("click", () => {
+        if (isSubscribed) removeSubscribe(companyId);
+        else pushSubscribe(companyId);
+        refillCompany();
+      });
+
+      newCellDOM.appendChild(newImgDOM);
+      newCellDOM.appendChild(newButtonDOM);
     }
-  }
-  else if (state.subscribeToggle === "my") {
-    for (let i = 0; i < companyPerPage; i++) {
-      const newCellDOM = document.createElement("div");
-      newCellDOM.setAttribute("class", "cell");
-      if (i < getSubscribeList().length) {
-        const newImgDOM = document.createElement("img");
-        newImgDOM.setAttribute("src", `../img/${getSubscribeList()[i]}.png`);
-        newCellDOM.appendChild(newImgDOM);
-      }
-      $gridDOM.appendChild(newCellDOM);
-    }
+    $gridDOM.appendChild(newCellDOM);
   }
 };
 
 export default function GridCompanySection(_newsCom) {
+  newsCompanyList = _newsCom;
+  refillCompany();
+
   document.querySelector(".toggle").addEventListener("click", (e) => {
     const { className: targetClassName } = e.target;
     if (targetClassName === "list-button" || targetClassName === "grid-button") {
@@ -53,7 +65,7 @@ export default function GridCompanySection(_newsCom) {
       updateDOMstyle($gridButtonDOM, { filter: isListButton ? "none" : blueStyle });
       updateDOMstyle(document.querySelector(".paper"), { display: isListButton ? "block" : "none" });
       updateDOMstyle(document.querySelector(".newsgroup"), { display: isListButton ? "flex" : "none" });
-      updateDOMstyle(document.querySelector(".grid-view"), { display: isListButton ? "none" : "grid" });
+      updateDOMstyle($gridDOM, { display: isListButton ? "none" : "grid" });
     }
     else if (targetClassName === "whole" || targetClassName === "my") {
       if (targetClassName !== state.subscribeToggle) {
@@ -63,7 +75,4 @@ export default function GridCompanySection(_newsCom) {
     }
   });
 
-  newsCompanyList = _newsCom;
-
-  refillCompany();
 }
