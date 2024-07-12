9c86cceafb93d34991ff569834837348bb996763
pccommen <pccommen@gmail.com>
2024-07-12 10:41:43 +0900

    refactor: navControl.js 삭제에 따른 module import 제거

--- frontend/static/js/component/mainContent/index.js
+++ frontend/static/js/component/mainContent/index.js
@@ -1,5 +1,4 @@
 import { removeAllChild } from "../../utils/removeAllChild.js"
-import * as navControl from "./navControl.js"
 
 const id = "mainContent"
 let state = {
@@ -225,8 +224,24 @@ function restartProgress() {
     contentInit(state.sortedNews)
     const element = getSelectedNav();
     const navElements = getAllNavElements();
-    navControl.removeSelected(navElements);
-    navControl.selected(state.currentArticleDetailIndex, state.sortedNews[state.currentArticleIndex].length, element);
+    removeSelected(navElements);
+    selected(state.currentArticleDetailIndex, state.sortedNews[state.currentArticleIndex].length, element);
+}
+
+function removeSelected(navElements) {
+    document.querySelectorAll(".mainContentNavCounter").forEach((element) => { element.remove(); });
+    if (document.querySelector(".mainContentNavProgress") != null) document.querySelector(".mainContentNavProgress").remove();
+    document.querySelectorAll(".absolute").forEach((element) => { element.classList.remove("absolute"); });
+    navElements.forEach(element => {
+        element.classList.remove("isSelected");
+    });
+}
+
+function selected(index, totalIndex, selectedDom) {
+    selectedDom.classList.add("isSelected");
+    document.querySelector(".mainContentNavElement.isSelected>#text").classList.add("absolute");
+    const template = `<div class="mainContentNavProgress"></div><span class="mainContentNavCounter" style="margin-left: 100px; opacity:99%;">${index + 1}</span><span class="mainContentNavCounter" style="opacity: 70%; margin-left:10px;"> / ${totalIndex}</span>`
+    selectedDom.insertAdjacentHTML('beforeend', template);
 }
 
 
